4f99259316943840eeef8a1b5612ff5ad0888fa9
Alex Kong <alex.lt.kong@connect.ust.hk>
2021-10-13 17:35:15 +0800

    add int to char

--- int-to-char/int-to-char.c
+++ int-to-char/int-to-char.c
@@ -2,8 +2,11 @@
 
 int main(){
 
+  FILE *fp;
+  fp = fopen("int-to-char.out","w");
   for (int i=70; i<80; i++) {
     printf("as int: %d, as char: %c, addr: %ld\n", i, i, &i);
+    fprintf(fp, "as int: %d, as char: %c, addr: %ld\n", i, i, &i);
     // addr canNOT use %d, possibly because 64bit machines need more than int
     // to display the address.
   }
