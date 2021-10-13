8b44d38d28e687edfd18ee2db4af871c1971ad8a
Alex Kong <alex.lt.kong@connect.ust.hk>
2021-10-13 17:22:37 +0800

    write to file

--- integer-overflow/integer-overflow.c
+++ integer-overflow/integer-overflow.c
@@ -3,11 +3,15 @@
 int main(){
 
   short a = 32765;
+  FILE *fp;
+  fp = fopen("integer-overflow.out","w");
 
   for (int i=0; i<5; i++) {
     a++;
     printf("%d\n", a);
+    fprintf(fp,"%d\n",a);
   }
 
+  fclose(fp);
   return 0;
 }
