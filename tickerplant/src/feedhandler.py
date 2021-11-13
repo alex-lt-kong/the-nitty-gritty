76e82d173499441c72e8c234949a146126c55fd3
Alex Kong <alex.lt.kong@connect.ust.hk>
2021-11-13 15:55:04 +0800

    parameterization

--- tickerplant/src/feedhandler.py
+++ tickerplant/src/feedhandler.py
@@ -4,13 +4,22 @@ import time
 import random
 
 q = qconnection.QConnection(host="localhost", port=9526, username='', password='')
+# connect to a q process, so that we can execute q statements in Python
 q.open()
 print(q)
+print(f'IPC version: {q.protocol_version}. Is connected: {q.is_connected()}')
 
 q("h:neg hopen `:localhost:9527;")
 tickers = ["HSBC", "IBM", "BABA"]
 count = 0
 while True:
-  q("h(`upd;`quote;(.z.N;`HSBC;123.45;543.21));")
+  q.sendSync(
+    """{[bidPrice;askPrice]
+        h(`upd;`quote;(.z.N;`HSBC;bidPrice;askPrice));}
+    """,
+    random.random()*100,
+    -random.random()*100
+  )
+  time.sleep(100)
 
 q.close()
