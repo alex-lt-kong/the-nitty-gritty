8bef583b38f1ebdf9e7c2fa743166ba621e9319c
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-02-12 20:24:18 +0800

    async and await

--- javascript/promises-and-async-await/6_async-func-eats-your-exceptions.js
+++ javascript/promises-and-async-await/6_async-func-eats-your-exceptions.js
@@ -9,28 +9,17 @@ function add(a, b) {
   })
 }
 
-console.log('BEFORE Promise()');
-add(1, 2).then((sum) => {     
-  console.log(sum);
-  return add(sum, 4);
-}).then(
-  sum => console.log(sum)
-).catch((e) => { 
-  console.log(e) 
-});
-console.log('AFTER Promise()');
 
 console.log('BEFORE async doubleAdd()');
 async function doubleAdd(a, b, c) {
-  try {
-    let sum0 = await add(a, b);
-    console.log(sum0);
-    let sum1 = await add(sum0, c);
-    console.log(sum1);
-
-  } catch (error) {
-    console.log(error);
-  }
+  let sum0 = await add(a, b);
+  console.log(sum0);
+  let sum1 = await add(sum0, c);
+  console.log(sum1);
+  // In the previous example, these lines are enclosed in a try...catch block
+  // but here they aren't. In the past, the exception here will be silently
+  // swallowed. But seems JavaScript evolved a bit since then and now we
+  // get a message for that.
 }
-doubleAdd(1, 2, 4);
+doubleAdd(1, 2, -1);
 console.log('AFTER async doubleAdd()');
