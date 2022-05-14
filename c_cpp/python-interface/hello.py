0b2059aa268d21355ec905ba2f6aa8fcf4cd5382
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-05-14 17:32:19 +0800

    C++ python interface

--- c_cpp/python-interface/hello.py
+++ c_cpp/python-interface/hello.py
@@ -4,10 +4,14 @@ import numpy as np
 
 limit = 5_000_000
 
+print('Numpy vectorization')
 start = time.time()
 print(np.sum(np.arange(limit)))
 print(f'{time.time() - start:.6} sec')
 
+print()
+
+print('C++ naive loop')
 start = time.time()
 hello.sum_a_list(limit)
 print(f'{time.time() - start:.6} sec')
