8f58eadd2672244515312ac204ee38e7fdc20106
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-07-20 11:47:50 +0800

    naive benchmarking

--- c/python-module/func.c
+++ c/python-module/func.c
@@ -23,3 +23,30 @@ int bubble_sort_inplace(int *arr, size_t arr_len) {
     }
     return 0;
 }
+
+
+int partition(int *arr, int lo, int hi) {
+    int pos = lo - 1;
+    int pivot = arr[hi];
+    for (int i = lo; i <= hi; ++i) {
+        if (arr[i] < pivot) {
+            ++pos;
+            int t = arr[i];
+            arr[i] = arr[pos];
+            arr[pos] = t;
+        }
+    }
+    ++pos;
+    int t = arr[hi];
+    arr[hi] = arr[pos];
+    arr[pos] = t;
+    return pos;
+}
+
+int quick_sort_inplace(int* arr, int lo, int hi) {
+    if (lo < hi) {
+        int pos = partition(arr, lo, hi);
+        quick_sort_inplace(arr, lo, pos - 1);
+        quick_sort_inplace(arr, pos + 1, hi);
+    }
+}
--- c/python-module/main.py
+++ c/python-module/main.py
@@ -3,6 +3,7 @@ from numpy.ctypeslib import ndpointer
 import ctypes
 import numpy as np
 import numpy.typing as npt
+import time
 import typing
 
 
@@ -33,6 +34,32 @@ def bubble_sort_inplace(arr: npt.NDArray[np.int32]) -> None:
         print('C function returns non-zero value!')
 
 
+def simple_benchmarking() -> None:
+    iters = 16
+    size = 4 * 1024 * 1024
+
+    funcs.quick_sort_inplace.argtypes = [
+        ndpointer(ctypes.c_int, flags="C_CONTIGUOUS"),
+        ctypes.c_size_t,
+        ctypes.c_size_t
+    ]
+    for i in range(iters):
+        arr_np = np.random.randint(0, 2147483648, size, dtype=np.int32)  
+        arr_c = arr_np.copy()
+        start_time = time.time()
+        arr_np.sort()
+        print(f'{time.time()- start_time:.3f} sec')
+
+        start_time = time.time()
+        funcs.quick_sort_inplace(arr_c, 0, arr_c.shape[0] - 1)
+        print(f'{time.time()- start_time:.3f} sec')
+
+        np.testing.assert_array_equal(arr_np, arr_c)
+
+
+
 print(f'funcs.square(10): {funcs.square(10)}')
 print(f'sum_an_array([3, 1, 4, 1, 5]): {sum_an_array([3, 1, 4, 1, 5])}')
 bubble_sort_inplace(np.array([3, 1, 4, 1, 5, 9, 2, 6], dtype=np.int32))
+
+simple_benchmarking()
