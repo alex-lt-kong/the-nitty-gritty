a68fc0560aeacbdbe0df8ffb87c21e6f1d5272e8
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-08-01 11:54:19 +0800

    more on dict

--- c/14_dictionary/dict.c
+++ c/14_dictionary/dict.c
@@ -1,49 +1,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct nlist { /* table entry: */
-    struct nlist *next; /* next entry in chain */
-    char *name; /* defined name */
-    char *defn; /* replacement text */
+/* finalization is not done here, memory will leak. */
+
+struct dict_item { /* table entry: */
+    struct dict_item *next; /* next item in a linked list of items with the SAME hash value */
+    char *key; /* defined name */
+    char *value; /* replacement text */
 };
 
 #define HASHSIZE 101
-static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+static struct dict_item *hashtab[HASHSIZE]; /* pointer table */
 
 /* hash: form hash value for string s */
 unsigned hash(char *s)
 {
+    // This simple hash algorithm is from C Programming Language by Brian W. Kernighan and Dennis M. Ritchie
+    // It is sometimes referred as "the K&R C hashing algorithm"
+    // https://stackoverflow.com/questions/4384359/quick-way-to-implement-dictionary-in-c
+
     unsigned hashval;
-    for (hashval = 0; *s != '\0'; s++)
+    for (hashval = 0; *s != '\0'; s++) {
       hashval = *s + 31 * hashval;
+    }
     return hashval % HASHSIZE;
 }
 
 /* lookup: look for s in hashtab */
-struct nlist *lookup(char *s)
+struct dict_item *lookup(char *s)
 {
-    struct nlist *np;
-    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-        if (strcmp(s, np->name) == 0)
-          return np; /* found */
+    struct dict_item *np;
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next) {
+    /* It is a bit confusing on why we need to follow the linked list. The reason is that, the linked list is NOT
+    used to link all the element in the dictionary. Instead, it is only used to link the elements with the same hashval, 
+    i.e., the elements whose names cause hash collision
+    */
+        if (strcmp(s, np->key) == 0) {
+            return np; /* found */
+        }
+    }
     return NULL; /* not found */
 }
 
-/* install: put (name, defn) in hashtab */
-struct nlist *install(char *name, char *defn)
+/* install: put (key, value) in hashtab */
+struct dict_item *install(char *key, char *value)
 {
-    struct nlist *np;
+    struct dict_item *np;
     unsigned hashval;
-    if ((np = lookup(name)) == NULL) { /* not found */
-        np = (struct nlist *) malloc(sizeof(*np));
-        if (np == NULL || (np->name = strdup(name)) == NULL)
+    if ((np = lookup(key)) == NULL) { /* not found */
+        np = (struct dict_item *) malloc(sizeof(*np));
+        if (np == NULL || (np->key = strdup(key)) == NULL)
           return NULL;
-        hashval = hash(name);
+        hashval = hash(key);
+        // we prepend the new node to the linked list of elements with the same hash value.
         np->next = hashtab[hashval];
         hashtab[hashval] = np;
-    } else /* already there */
-        free((void *) np->defn); /*free previous defn */
-    if ((np->defn = strdup(defn)) == NULL)
+    } else {/* already there */
+        free((void *) np->value); /*free previous value */
+    }
+    if ((np->value = strdup(value)) == NULL)
        return NULL;
     return np;
 }
--- c/14_dictionary/main.c
+++ c/14_dictionary/main.c
@@ -4,13 +4,15 @@
 
 int main() {
     install("hello", "world!");
-    install("foo", "bar");
+    install("foo", "bar");    
     install("pi", "3.1415926535");
-    printf("%s\n", lookup("hello")->defn);
-    printf("%s\n", lookup("foo")->defn);
-    printf("%s\n", lookup("foo")->defn);    
-    printf("%s\n", lookup("pi")->defn);    
-    printf("%s\n", lookup("foo")->defn);
-    printf("%s\n", lookup("foo")->defn);
+
+    printf("%s\n", lookup("hello")->value);
+    printf("%s\n", lookup("foo")->value);
+    printf("%s\n", lookup("foo")->value);    
+    printf("%s\n", lookup("pi")->value);
+    install("foo", "new bar");
+    printf("%s\n", lookup("foo")->value);
+    printf("%s\n", lookup("foo")->value);
     return 0;
 }
