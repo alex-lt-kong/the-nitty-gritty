60e9726fbfe6f0f0a294763bbc18045a4ce1d078
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-08-08 19:48:05 +0800

    more on disassembly

--- c/12_disassembly/3_division/README.md
+++ c/12_disassembly/3_division/README.md
@@ -1,3 +1,7 @@
 # Division
 
 * `divide_by_2.asm`: `gcc` is smart enough to convert `divide by 2` to shift the operand right by one bit.
+
+* `divide_by_constant.asm`: `gcc` is smart enough to convert `divide by 13` to firstly multiply by 1,321,528,399 and
+then right shift the result by 34 bits. Painstakingly writing `a / 13` as `a * 0.07692307692307693` is unlikely
+able to make the code faster.
--- c/12_disassembly/3_division/divide_by_constant.asm
+++ c/12_disassembly/3_division/divide_by_constant.asm
@@ -1,11 +1,14 @@
 for function divide_by_constant:
 
 
-int result = a / 271;
-<+0>:	movsxd rax,edi
-<+3>:	sar    edi,0x1f
-<+6>:	imul   rax,rax,0xf1d48bd
-<+13>:	sar    rax,0x24
+int result = a / 13;
+<+0>:	movsxd rax,edi   ; cast 32-bit int a to 64-bit int.
+<+3>:	sar    edi,0x1f  ; 0x1f == 31
+; Shift Arithmetic Right. It is almost the same as SHR, except that most-significant bit (MSB) is shifted back to itself.
+; This preserves the original sign of the destination operand, because MSB is the sign bit
+<+6>:	imul   rax,rax,0x4ec4ec4f ; rax *= 1321528399
+<+13>:	sar    rax,0x22           ; rax /= 2^34
+; interestingly, 1321528399 / 2^34 == 1 / 13 == 0.07692307692307693
 <+17>:	sub    eax,edi
 
 return result;
