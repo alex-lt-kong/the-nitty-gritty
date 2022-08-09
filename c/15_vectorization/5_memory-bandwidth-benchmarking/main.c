82616918eb17d4ced87d0f08000fd3057d4b11e4
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-08-09 15:44:46 +0800

    more on memory bandwidth benchmarking

--- c/15_vectorization/5_memory-bandwidth-benchmarking/main.c
+++ c/15_vectorization/5_memory-bandwidth-benchmarking/main.c
@@ -7,7 +7,6 @@
 
 #define SIZE_KB {8, 16, 24, 28, 32, 36, 40, 48, 64, 128, 256, 384, 512, 768, 1024, 1025, 2048, 4096, 8192, 16384, 200000}
 #define TEST_MEM_IN_BYTES 10737418240 // 10 * 1024 * 1024 * 1024 = 10GB
-#define BUFFERS 1
 
 double timer(void)
 {
@@ -22,13 +21,13 @@ double timer(void)
 
 int main(int argc, char **argv)
 {
-    double *x[BUFFERS];
+    double *x;
     uint64_t t1, t2;
     int kbsizes[] = SIZE_KB;
     size_t bsizes[] = SIZE_KB;
     double bandwidth[sizeof(kbsizes)/sizeof(int)];
     int iterations[sizeof(kbsizes)/sizeof(int)];
-    double *address[sizeof(kbsizes)/sizeof(int)][BUFFERS];
+    double *address[sizeof(kbsizes)/sizeof(int)];
 
 
 
@@ -40,35 +39,27 @@ int main(int argc, char **argv)
     for (int i = 0; i < sizeof(kbsizes)/sizeof(int); i++)
     {
         // Allocate
-        for (int k = 0; k < BUFFERS; k++) {
-            x[k] = (double *) malloc(bsizes[i]);
-            address[i][k] = x[k];
-            memset(x[k], 0, bsizes[i]);
-        }
+        x = (double *) malloc(bsizes[i]);
+        address[i] = x;
+        memset(x, 0, bsizes[i]);
+        
 
         // Measure
         t1 = get_timestamp_in_microsec();
         for (int j = 0; j < iterations[i]; ++j) {
-            for (int k = 0; k < BUFFERS; ++k) {
-                memset(x[k], 0xff, bsizes[i]);
-            }
+            memset(x, 0xff, bsizes[i]);
         }
         t2 = get_timestamp_in_microsec();
-        bandwidth[i] = (BUFFERS*kbsizes[i]*iterations[i]) / 1024.0 / 1024.0 * 1000 * 1000 / (t2-t1);
+        bandwidth[i] = (kbsizes[i]*iterations[i]) / 1024.0 / 1024.0 * 1000 * 1000 / (t2-t1);
 
-        // Free
-        for (int k = 0; k < BUFFERS; k++)
-            free(x[k]);
+        free(x);
     }
 
-    printf("TESTMEM = %ld\n", TEST_MEM_IN_BYTES);
-    printf("BUFFERS = %d\n", BUFFERS);
+    printf("TESTMEM = %ld MB\n", TEST_MEM_IN_BYTES / 1024 / 1024);
     printf("Size (kB)\tBandwidth (GB/s)\tIterations\tAddresses\n");
-    for (int l = 0; l < sizeof(kbsizes)/sizeof(int); l++)
+    for (int i = 0; i < sizeof(kbsizes)/sizeof(int); i++)
     {
-        printf("%7d\t\t%.2f\t\t\t%d\t\t%x", kbsizes[l], bandwidth[l], iterations[l], address[l][0]);
-        for (int k = 1; k < BUFFERS; k++)
-            printf(", %x", address[l][k]);
+        printf("%7d\t\t%.2f\t\t\t%d\t\t%x", kbsizes[i], bandwidth[i], iterations[i], address[i]);
         printf("\n");
     }
 
