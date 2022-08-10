ad574435cd93433d7062f5f032966be564e2996e
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-08-10 22:13:00 +0800

    re-generate asm code

--- c/15_vectorization/7_soa-vs-aos/func-gcc-no.asm
+++ c/15_vectorization/7_soa-vs-aos/func-gcc-no.asm
@@ -1,94 +1,94 @@
-for function floating_division_aos:
-b, float c, struct pixel** arr, struct pixel** results, size_t arr_len) {
-<+0>:	movaps xmm3,xmm0
-<+3>:	mov    r8,rsi
+Dump of assembler code for function floating_division_aos:
+21	void floating_division_aos(float a, float b, float c, struct pixel** arr, struct pixel** results, size_t arr_len) {
+   0x0000000000000000 <+0>:	movaps xmm3,xmm0
+   0x0000000000000003 <+3>:	mov    r8,rsi
 
+22	  
+23	  #if defined( __INTEL_COMPILER)
+24	  #pragma ivdep
+25	  // Pragmas are specific for the compiler and platform in use. So the best bet is to look at compiler's documentation.
+26	  // https://stackoverflow.com/questions/5078679/what-is-the-scope-of-a-pragma-directive
+27	  #elif defined(__GNUC__)
+28	  #pragma GCC ivdep
+29	  #endif
+30	  for (int i = 0; i < arr_len; ++i) {
+   0x0000000000000006 <+6>:	test   rdx,rdx
+   0x0000000000000009 <+9>:	je     0x50 <floating_division_aos+80>
+   0x000000000000000b <+11>:	lea    rsi,[rdx*8+0x0]
+   0x0000000000000013 <+19>:	xor    eax,eax
+   0x0000000000000015 <+21>:	nop    DWORD PTR [rax]
+   0x0000000000000020 <+32>:	add    rax,0x8
+   0x000000000000004b <+75>:	cmp    rsi,rax
+   0x000000000000004e <+78>:	jne    0x18 <floating_division_aos+24>
 
-__INTEL_COMPILER)
+31	    results[i]->r = arr[i]->r / a;
+   0x0000000000000018 <+24>:	mov    rcx,QWORD PTR [rdi+rax*1]
+   0x000000000000001c <+28>:	mov    rdx,QWORD PTR [r8+rax*1]
+   0x0000000000000024 <+36>:	movss  xmm0,DWORD PTR [rcx]
+   0x0000000000000028 <+40>:	divss  xmm0,xmm3
+   0x000000000000002c <+44>:	movss  DWORD PTR [rdx],xmm0
 
-are specific for the compiler and platform in use. So the best bet is to look at compiler's documentation.
+32	    results[i]->g = b / arr[i]->g;
+   0x0000000000000030 <+48>:	movaps xmm0,xmm1
+   0x0000000000000033 <+51>:	divss  xmm0,DWORD PTR [rcx+0x4]
+   0x0000000000000038 <+56>:	movss  DWORD PTR [rdx+0x4],xmm0
 
+33	    results[i]->b = arr[i]->b / c;
+   0x000000000000003d <+61>:	movss  xmm0,DWORD PTR [rcx+0x8]
+   0x0000000000000042 <+66>:	divss  xmm0,xmm2
+   0x0000000000000046 <+70>:	movss  DWORD PTR [rdx+0x8],xmm0
 
-ivdep
-
-i = 0; i < arr_len; ++i) {
-<+6>:	test   rdx,rdx
-<+9>:	je     0x50 <floating_division_aos+80>
-<+11>:	lea    rsi,[rdx*8+0x0]
-<+19>:	xor    eax,eax
-<+21>:	nop    DWORD PTR [rax]
-<+32>:	add    rax,0x8
-<+75>:	cmp    rsi,rax
-<+78>:	jne    0x18 <floating_division_aos+24>
-
-results[i]->r = arr[i]->r / a;
-<+24>:	mov    rcx,QWORD PTR [rdi+rax*1]
-<+28>:	mov    rdx,QWORD PTR [r8+rax*1]
-<+36>:	movss  xmm0,DWORD PTR [rcx]
-<+40>:	divss  xmm0,xmm3
-<+44>:	movss  DWORD PTR [rdx],xmm0
-
-results[i]->g = b / arr[i]->g;
-<+48>:	movaps xmm0,xmm1
-<+51>:	divss  xmm0,DWORD PTR [rcx+0x4]
-<+56>:	movss  DWORD PTR [rdx+0x4],xmm0
-
-results[i]->b = arr[i]->b / c;
-<+61>:	movss  xmm0,DWORD PTR [rcx+0x8]
-<+66>:	divss  xmm0,xmm2
-<+70>:	movss  DWORD PTR [rdx+0x8],xmm0
-
-
+34	  }
 35	}
-<+80>:	ret    
-nop WORD PTR cs:[rax+rax*1+0x0]
-   DWORD PTR [rax+0x0]
+   0x0000000000000050 <+80>:	ret    
+   0x0000000000000051:	data16 nop WORD PTR cs:[rax+rax*1+0x0]
+   0x000000000000005c:	nop    DWORD PTR [rax+0x0]
 
+End of assembler dump.
+Dump of assembler code for function floating_division_soa:
+37	void floating_division_soa(float a, float b, float c, struct pixelArray* arr, struct pixelArray* results, size_t arr_len) {
+   0x0000000000000060 <+0>:	movaps xmm3,xmm0
+   0x0000000000000063 <+3>:	mov    rcx,rdi
+   0x0000000000000066 <+6>:	mov    rax,rsi
 
-for function floating_division_soa:
-b, float c, struct pixelArray* arr, struct pixelArray* results, size_t arr_len) {
-<+0>:	movaps xmm3,xmm0
-<+3>:	mov    rcx,rdi
-<+6>:	mov    rax,rsi
+38	  #if defined( __INTEL_COMPILER)
+39	  #pragma ivdep
+40	  #elif defined(__GNUC__)
+41	  #pragma GCC ivdep
+42	  #endif
+43	  for (int i = 0; i < arr_len; ++i) {
+   0x0000000000000069 <+9>:	test   rdx,rdx
+   0x000000000000006c <+12>:	je     0xc5 <floating_division_soa+101>
+   0x00000000000000bc <+92>:	add    rax,0x4
+   0x00000000000000c0 <+96>:	cmp    rdx,rax
+   0x00000000000000c3 <+99>:	jne    0x90 <floating_division_soa+48>
 
-__INTEL_COMPILER)
+44	    results->r[i] = arr->r[i] / a;
+   0x000000000000006e <+14>:	mov    r10,QWORD PTR [rdi]
+   0x0000000000000071 <+17>:	mov    r9,QWORD PTR [rsi]
+   0x0000000000000074 <+20>:	shl    rdx,0x2
+   0x0000000000000090 <+48>:	movss  xmm0,DWORD PTR [r10+rax*1]
+   0x0000000000000096 <+54>:	divss  xmm0,xmm3
+   0x000000000000009a <+58>:	movss  DWORD PTR [r9+rax*1],xmm0
 
+45	    results->g[i] = b / arr->g[i];
+   0x0000000000000078 <+24>:	mov    r8,QWORD PTR [rdi+0x8]
+   0x000000000000007c <+28>:	mov    rdi,QWORD PTR [rsi+0x8]
+   0x00000000000000a0 <+64>:	movaps xmm0,xmm1
+   0x00000000000000a3 <+67>:	divss  xmm0,DWORD PTR [r8+rax*1]
+   0x00000000000000a9 <+73>:	movss  DWORD PTR [rdi+rax*1],xmm0
 
-ivdep
+46	    results->b[i] = arr->b[i] / c;
+   0x0000000000000080 <+32>:	mov    rsi,QWORD PTR [rcx+0x10]
+   0x0000000000000084 <+36>:	mov    rcx,QWORD PTR [rax+0x10]
+   0x0000000000000088 <+40>:	xor    eax,eax
+   0x000000000000008a <+42>:	nop    WORD PTR [rax+rax*1+0x0]
+   0x00000000000000ae <+78>:	movss  xmm0,DWORD PTR [rsi+rax*1]
+   0x00000000000000b3 <+83>:	divss  xmm0,xmm2
+   0x00000000000000b7 <+87>:	movss  DWORD PTR [rcx+rax*1],xmm0
 
-i = 0; i < arr_len; ++i) {
-<+9>:	test   rdx,rdx
-<+12>:	je     0xc5 <floating_division_soa+101>
-<+92>:	add    rax,0x4
-<+96>:	cmp    rdx,rax
-<+99>:	jne    0x90 <floating_division_soa+48>
-
-results->r[i] = arr->r[i] / a;
-<+14>:	mov    r10,QWORD PTR [rdi]
-<+17>:	mov    r9,QWORD PTR [rsi]
-<+20>:	shl    rdx,0x2
-<+48>:	movss  xmm0,DWORD PTR [r10+rax*1]
-<+54>:	divss  xmm0,xmm3
-<+58>:	movss  DWORD PTR [r9+rax*1],xmm0
-
-results->g[i] = b / arr->g[i];
-<+24>:	mov    r8,QWORD PTR [rdi+0x8]
-<+28>:	mov    rdi,QWORD PTR [rsi+0x8]
-<+64>:	movaps xmm0,xmm1
-<+67>:	divss  xmm0,DWORD PTR [r8+rax*1]
-<+73>:	movss  DWORD PTR [rdi+rax*1],xmm0
-
-results->b[i] = arr->b[i] / c;
-<+32>:	mov    rsi,QWORD PTR [rcx+0x10]
-<+36>:	mov    rcx,QWORD PTR [rax+0x10]
-<+40>:	xor    eax,eax
-<+42>:	nop    WORD PTR [rax+rax*1+0x0]
-<+78>:	movss  xmm0,DWORD PTR [rsi+rax*1]
-<+83>:	divss  xmm0,xmm2
-<+87>:	movss  DWORD PTR [rcx+rax*1],xmm0
-
-
+47	  }
 48	}
-<+101>:	ret    
+   0x00000000000000c5 <+101>:	ret    
 
-
+End of assembler dump.
--- c/15_vectorization/7_soa-vs-aos/func-icc-no.asm
+++ c/15_vectorization/7_soa-vs-aos/func-icc-no.asm
@@ -1,228 +1,228 @@
-for function floating_division_aos:
-b, float c, struct pixel** arr, struct pixel** results, size_t arr_len) {
+Dump of assembler code for function floating_division_aos:
+21	void floating_division_aos(float a, float b, float c, struct pixel** arr, struct pixel** results, size_t arr_len) {
 
+22	  
+23	  #if defined( __INTEL_COMPILER)
+24	  #pragma ivdep
+25	  // Pragmas are specific for the compiler and platform in use. So the best bet is to look at compiler's documentation.
+26	  // https://stackoverflow.com/questions/5078679/what-is-the-scope-of-a-pragma-directive
+27	  #elif defined(__GNUC__)
+28	  #pragma GCC ivdep
+29	  #endif
+30	  for (int i = 0; i < arr_len; ++i) {
+   0x0000000000000000 <+0>:	xor    eax,eax
+   0x0000000000000002 <+2>:	test   rdx,rdx
+   0x0000000000000005 <+5>:	jbe    0x41 <floating_division_aos+65>
+   0x0000000000000012 <+18>:	inc    rax
+   0x000000000000003c <+60>:	cmp    rax,rdx
+   0x000000000000003f <+63>:	jb     0x7 <floating_division_aos+7>
 
-__INTEL_COMPILER)
+31	    results[i]->r = arr[i]->r / a;
+   0x0000000000000007 <+7>:	mov    rcx,QWORD PTR [rdi+rax*8]
+   0x000000000000000e <+14>:	mov    r8,QWORD PTR [rsi+rax*8]
+   0x0000000000000015 <+21>:	movss  xmm3,DWORD PTR [rcx]
+   0x0000000000000023 <+35>:	divss  xmm3,xmm0
+   0x000000000000002b <+43>:	movss  DWORD PTR [r8],xmm3
 
-are specific for the compiler and platform in use. So the best bet is to look at compiler's documentation.
+32	    results[i]->g = b / arr[i]->g;
+   0x000000000000000b <+11>:	movaps xmm4,xmm1
+   0x000000000000001e <+30>:	divss  xmm4,DWORD PTR [rcx+0x4]
+   0x0000000000000030 <+48>:	movss  DWORD PTR [r8+0x4],xmm4
 
+33	    results[i]->b = arr[i]->b / c;
+   0x0000000000000019 <+25>:	movss  xmm5,DWORD PTR [rcx+0x8]
+   0x0000000000000027 <+39>:	divss  xmm5,xmm2
+   0x0000000000000036 <+54>:	movss  DWORD PTR [r8+0x8],xmm5
 
-ivdep
-
-i = 0; i < arr_len; ++i) {
-<+0>:	xor    eax,eax
-<+2>:	test   rdx,rdx
-<+5>:	jbe    0x41 <floating_division_aos+65>
-<+18>:	inc    rax
-<+60>:	cmp    rax,rdx
-<+63>:	jb     0x7 <floating_division_aos+7>
-
-results[i]->r = arr[i]->r / a;
-<+7>:	mov    rcx,QWORD PTR [rdi+rax*8]
-<+14>:	mov    r8,QWORD PTR [rsi+rax*8]
-<+21>:	movss  xmm3,DWORD PTR [rcx]
-<+35>:	divss  xmm3,xmm0
-<+43>:	movss  DWORD PTR [r8],xmm3
-
-results[i]->g = b / arr[i]->g;
-<+11>:	movaps xmm4,xmm1
-<+30>:	divss  xmm4,DWORD PTR [rcx+0x4]
-<+48>:	movss  DWORD PTR [r8+0x4],xmm4
-
-results[i]->b = arr[i]->b / c;
-<+25>:	movss  xmm5,DWORD PTR [rcx+0x8]
-<+39>:	divss  xmm5,xmm2
-<+54>:	movss  DWORD PTR [r8+0x8],xmm5
-
-
+34	  }
 35	}
-<+65>:	ret    
-<+66>:	nop    DWORD PTR [rax+0x0]
-<+73>:	nop    DWORD PTR [rax+0x0]
+   0x0000000000000041 <+65>:	ret    
+   0x0000000000000042 <+66>:	nop    DWORD PTR [rax+0x0]
+   0x0000000000000049 <+73>:	nop    DWORD PTR [rax+0x0]
 
+End of assembler dump.
+Dump of assembler code for function floating_division_soa:
+37	void floating_division_soa(float a, float b, float c, struct pixelArray* arr, struct pixelArray* results, size_t arr_len) {
+   0x0000000000000050 <+0>:	push   r14
+   0x000000000000005b <+11>:	mov    rax,rdx
+   0x000000000000007b <+43>:	shr    rax,0x3
+   0x0000000000000256 <+518>:	sub    rdx,rdi
+   0x0000000000000259 <+521>:	inc    rdx
+   0x000000000000025c <+524>:	dec    edx
 
-for function floating_division_soa:
-b, float c, struct pixelArray* arr, struct pixelArray* results, size_t arr_len) {
-<+0>:	push   r14
-<+11>:	mov    rax,rdx
-<+43>:	shr    rax,0x3
-<+518>:	sub    rdx,rdi
-<+521>:	inc    rdx
-<+524>:	dec    edx
+38	  #if defined( __INTEL_COMPILER)
+39	  #pragma ivdep
+40	  #elif defined(__GNUC__)
+41	  #pragma GCC ivdep
+42	  #endif
+43	  for (int i = 0; i < arr_len; ++i) {
+   0x0000000000000052 <+2>:	test   rdx,rdx
+   0x0000000000000055 <+5>:	jbe    0x3c8 <floating_division_soa+888>
+   0x000000000000005e <+14>:	xor    ecx,ecx
+   0x0000000000000076 <+38>:	mov    edi,0x1
+   0x000000000000007f <+47>:	je     0x24d <floating_division_soa+509>
+   0x000000000000009a <+74>:	inc    ecx
+   0x000000000000023b <+491>:	cmp    rcx,rax
+   0x000000000000023e <+494>:	jb     0x85 <floating_division_soa+53>
+   0x0000000000000244 <+500>:	shl    ecx,0x3
+   0x0000000000000247 <+503>:	movsxd rdi,ecx
+   0x000000000000024a <+506>:	inc    rdi
+   0x000000000000024d <+509>:	cmp    rdi,rdx
+   0x0000000000000250 <+512>:	ja     0x3c8 <floating_division_soa+888>
+   0x000000000000025e <+526>:	jmp    QWORD PTR [rdx*8+0x0]
 
-__INTEL_COMPILER)
+44	    results->r[i] = arr->r[i] / a;
+   0x0000000000000060 <+16>:	mov    r10,QWORD PTR [rsi]
+   0x000000000000006b <+27>:	mov    r11,QWORD PTR [rdi]
+   0x0000000000000085 <+53>:	movsxd rdi,ecx
+   0x000000000000008b <+59>:	shl    rdi,0x5
+   0x000000000000009c <+76>:	movss  xmm3,DWORD PTR [r11+rdi*1]
+   0x00000000000000a2 <+82>:	divss  xmm3,xmm0
+   0x00000000000000a6 <+86>:	movss  DWORD PTR [r10+rdi*1],xmm3
+   0x00000000000000ca <+122>:	movss  xmm6,DWORD PTR [r11+rdi*1+0x4]
+   0x00000000000000d1 <+129>:	divss  xmm6,xmm0
+   0x00000000000000d5 <+133>:	movss  DWORD PTR [r10+rdi*1+0x4],xmm6
+   0x0000000000000100 <+176>:	movss  xmm9,DWORD PTR [r11+rdi*1+0x8]
+   0x0000000000000107 <+183>:	divss  xmm9,xmm0
+   0x000000000000010c <+188>:	movss  DWORD PTR [r10+rdi*1+0x8],xmm9
+   0x0000000000000138 <+232>:	movss  xmm12,DWORD PTR [r11+rdi*1+0xc]
+   0x000000000000013f <+239>:	divss  xmm12,xmm0
+   0x0000000000000144 <+244>:	movss  DWORD PTR [r10+rdi*1+0xc],xmm12
+   0x0000000000000170 <+288>:	movss  xmm15,DWORD PTR [r11+rdi*1+0x10]
+   0x0000000000000177 <+295>:	divss  xmm15,xmm0
+   0x000000000000017c <+300>:	movss  DWORD PTR [r10+rdi*1+0x10],xmm15
+   0x00000000000001a2 <+338>:	movss  xmm5,DWORD PTR [r11+rdi*1+0x14]
+   0x00000000000001a9 <+345>:	divss  xmm5,xmm0
+   0x00000000000001ad <+349>:	movss  DWORD PTR [r10+rdi*1+0x14],xmm5
+   0x00000000000001d3 <+387>:	movss  xmm8,DWORD PTR [r11+rdi*1+0x18]
+   0x00000000000001da <+394>:	divss  xmm8,xmm0
+   0x00000000000001df <+399>:	movss  DWORD PTR [r10+rdi*1+0x18],xmm8
+   0x0000000000000207 <+439>:	movss  xmm11,DWORD PTR [r11+rdi*1+0x1c]
+   0x000000000000020e <+446>:	divss  xmm11,xmm0
+   0x0000000000000213 <+451>:	movss  DWORD PTR [r10+rdi*1+0x1c],xmm11
+   0x0000000000000265 <+533>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x14]
+   0x000000000000026f <+543>:	divss  xmm3,xmm0
+   0x0000000000000273 <+547>:	movss  DWORD PTR [r10+rdi*4+0x14],xmm3
+   0x0000000000000299 <+585>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x10]
+   0x00000000000002a3 <+595>:	divss  xmm3,xmm0
+   0x00000000000002a7 <+599>:	movss  DWORD PTR [r10+rdi*4+0x10],xmm3
+   0x00000000000002cd <+637>:	movss  xmm3,DWORD PTR [r11+rdi*4+0xc]
+   0x00000000000002d7 <+647>:	divss  xmm3,xmm0
+   0x00000000000002db <+651>:	movss  DWORD PTR [r10+rdi*4+0xc],xmm3
+   0x0000000000000301 <+689>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x8]
+   0x000000000000030b <+699>:	divss  xmm3,xmm0
+   0x000000000000030f <+703>:	movss  DWORD PTR [r10+rdi*4+0x8],xmm3
+   0x0000000000000335 <+741>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x4]
+   0x000000000000033f <+751>:	divss  xmm3,xmm0
+   0x0000000000000343 <+755>:	movss  DWORD PTR [r10+rdi*4+0x4],xmm3
+   0x0000000000000369 <+793>:	movss  xmm3,DWORD PTR [r11+rdi*4]
+   0x0000000000000372 <+802>:	divss  xmm3,xmm0
+   0x0000000000000376 <+806>:	movss  DWORD PTR [r10+rdi*4],xmm3
+   0x0000000000000397 <+839>:	movss  xmm3,DWORD PTR [r11+rdi*4-0x4]
+   0x000000000000039e <+846>:	divss  xmm3,xmm0
+   0x00000000000003a2 <+850>:	movss  DWORD PTR [r10+rdi*4-0x4],xmm3
 
+45	    results->g[i] = b / arr->g[i];
+   0x0000000000000063 <+19>:	mov    r14,QWORD PTR [rsi+0x8]
+   0x000000000000006e <+30>:	mov    r9,QWORD PTR [rdi+0x8]
+   0x0000000000000088 <+56>:	movaps xmm4,xmm1
+   0x000000000000008f <+63>:	movaps xmm7,xmm1
+   0x0000000000000092 <+66>:	movaps xmm10,xmm1
+   0x0000000000000096 <+70>:	movaps xmm13,xmm1
+   0x00000000000000ac <+92>:	movaps xmm3,xmm1
+   0x00000000000000af <+95>:	divss  xmm4,DWORD PTR [r9+rdi*1]
+   0x00000000000000b5 <+101>:	movss  DWORD PTR [r14+rdi*1],xmm4
+   0x00000000000000dc <+140>:	movaps xmm6,xmm1
+   0x00000000000000df <+143>:	divss  xmm7,DWORD PTR [r9+rdi*1+0x4]
+   0x00000000000000e6 <+150>:	movss  DWORD PTR [r14+rdi*1+0x4],xmm7
+   0x0000000000000113 <+195>:	movaps xmm9,xmm1
+   0x0000000000000117 <+199>:	divss  xmm10,DWORD PTR [r9+rdi*1+0x8]
+   0x000000000000011e <+206>:	movss  DWORD PTR [r14+rdi*1+0x8],xmm10
+   0x000000000000014b <+251>:	movaps xmm12,xmm1
+   0x000000000000014f <+255>:	divss  xmm13,DWORD PTR [r9+rdi*1+0xc]
+   0x0000000000000156 <+262>:	movss  DWORD PTR [r14+rdi*1+0xc],xmm13
+   0x0000000000000183 <+307>:	divss  xmm3,DWORD PTR [r9+rdi*1+0x10]
+   0x000000000000018a <+314>:	movss  DWORD PTR [r14+rdi*1+0x10],xmm3
+   0x00000000000001b4 <+356>:	divss  xmm6,DWORD PTR [r9+rdi*1+0x14]
+   0x00000000000001bb <+363>:	movss  DWORD PTR [r14+rdi*1+0x14],xmm6
+   0x00000000000001e6 <+406>:	divss  xmm9,DWORD PTR [r9+rdi*1+0x18]
+   0x00000000000001ed <+413>:	movss  DWORD PTR [r14+rdi*1+0x18],xmm9
+   0x000000000000021a <+458>:	divss  xmm12,DWORD PTR [r9+rdi*1+0x1c]
+   0x0000000000000221 <+465>:	movss  DWORD PTR [r14+rdi*1+0x1c],xmm12
+   0x000000000000026c <+540>:	movaps xmm4,xmm1
+   0x000000000000027a <+554>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x14]
+   0x0000000000000281 <+561>:	movss  DWORD PTR [r14+rdi*4+0x14],xmm4
+   0x00000000000002a0 <+592>:	movaps xmm4,xmm1
+   0x00000000000002ae <+606>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x10]
+   0x00000000000002b5 <+613>:	movss  DWORD PTR [r14+rdi*4+0x10],xmm4
+   0x00000000000002d4 <+644>:	movaps xmm4,xmm1
+   0x00000000000002e2 <+658>:	divss  xmm4,DWORD PTR [r9+rdi*4+0xc]
+   0x00000000000002e9 <+665>:	movss  DWORD PTR [r14+rdi*4+0xc],xmm4
+   0x0000000000000308 <+696>:	movaps xmm4,xmm1
+   0x0000000000000316 <+710>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x8]
+   0x000000000000031d <+717>:	movss  DWORD PTR [r14+rdi*4+0x8],xmm4
+   0x000000000000033c <+748>:	movaps xmm4,xmm1
+   0x000000000000034a <+762>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x4]
+   0x0000000000000351 <+769>:	movss  DWORD PTR [r14+rdi*4+0x4],xmm4
+   0x000000000000036f <+799>:	movaps xmm4,xmm1
+   0x000000000000037c <+812>:	divss  xmm4,DWORD PTR [r9+rdi*4]
+   0x0000000000000382 <+818>:	movss  DWORD PTR [r14+rdi*4],xmm4
+   0x00000000000003a9 <+857>:	divss  xmm1,DWORD PTR [r9+rdi*4-0x4]
+   0x00000000000003b0 <+864>:	movss  DWORD PTR [r14+rdi*4-0x4],xmm1
 
-ivdep
+46	    results->b[i] = arr->b[i] / c;
+   0x0000000000000067 <+23>:	mov    r8,QWORD PTR [rsi+0x10]
+   0x0000000000000072 <+34>:	mov    rsi,QWORD PTR [rdi+0x10]
+   0x00000000000000bb <+107>:	movss  xmm5,DWORD PTR [rsi+rdi*1]
+   0x00000000000000c0 <+112>:	divss  xmm5,xmm2
+   0x00000000000000c4 <+116>:	movss  DWORD PTR [r8+rdi*1],xmm5
+   0x00000000000000ed <+157>:	movss  xmm8,DWORD PTR [rsi+rdi*1+0x4]
+   0x00000000000000f4 <+164>:	divss  xmm8,xmm2
+   0x00000000000000f9 <+169>:	movss  DWORD PTR [r8+rdi*1+0x4],xmm8
+   0x0000000000000125 <+213>:	movss  xmm11,DWORD PTR [rsi+rdi*1+0x8]
+   0x000000000000012c <+220>:	divss  xmm11,xmm2
+   0x0000000000000131 <+225>:	movss  DWORD PTR [r8+rdi*1+0x8],xmm11
+   0x000000000000015d <+269>:	movss  xmm14,DWORD PTR [rsi+rdi*1+0xc]
+   0x0000000000000164 <+276>:	divss  xmm14,xmm2
+   0x0000000000000169 <+281>:	movss  DWORD PTR [r8+rdi*1+0xc],xmm14
+   0x0000000000000191 <+321>:	movss  xmm4,DWORD PTR [rsi+rdi*1+0x10]
+   0x0000000000000197 <+327>:	divss  xmm4,xmm2
+   0x000000000000019b <+331>:	movss  DWORD PTR [r8+rdi*1+0x10],xmm4
+   0x00000000000001c2 <+370>:	movss  xmm7,DWORD PTR [rsi+rdi*1+0x14]
+   0x00000000000001c8 <+376>:	divss  xmm7,xmm2
+   0x00000000000001cc <+380>:	movss  DWORD PTR [r8+rdi*1+0x14],xmm7
+   0x00000000000001f4 <+420>:	movss  xmm10,DWORD PTR [rsi+rdi*1+0x18]
+   0x00000000000001fb <+427>:	divss  xmm10,xmm2
+   0x0000000000000200 <+432>:	movss  DWORD PTR [r8+rdi*1+0x18],xmm10
+   0x0000000000000228 <+472>:	movss  xmm13,DWORD PTR [rsi+rdi*1+0x1c]
+   0x000000000000022f <+479>:	divss  xmm13,xmm2
+   0x0000000000000234 <+484>:	movss  DWORD PTR [r8+rdi*1+0x1c],xmm13
+   0x0000000000000288 <+568>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x14]
+   0x000000000000028e <+574>:	divss  xmm5,xmm2
+   0x0000000000000292 <+578>:	movss  DWORD PTR [r8+rdi*4+0x14],xmm5
+   0x00000000000002bc <+620>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x10]
+   0x00000000000002c2 <+626>:	divss  xmm5,xmm2
+   0x00000000000002c6 <+630>:	movss  DWORD PTR [r8+rdi*4+0x10],xmm5
+   0x00000000000002f0 <+672>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0xc]
+   0x00000000000002f6 <+678>:	divss  xmm5,xmm2
+   0x00000000000002fa <+682>:	movss  DWORD PTR [r8+rdi*4+0xc],xmm5
+   0x0000000000000324 <+724>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x8]
+   0x000000000000032a <+730>:	divss  xmm5,xmm2
+   0x000000000000032e <+734>:	movss  DWORD PTR [r8+rdi*4+0x8],xmm5
+   0x0000000000000358 <+776>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x4]
+   0x000000000000035e <+782>:	divss  xmm5,xmm2
+   0x0000000000000362 <+786>:	movss  DWORD PTR [r8+rdi*4+0x4],xmm5
+   0x0000000000000388 <+824>:	movss  xmm5,DWORD PTR [rsi+rdi*4]
+   0x000000000000038d <+829>:	divss  xmm5,xmm2
+   0x0000000000000391 <+833>:	movss  DWORD PTR [r8+rdi*4],xmm5
+   0x00000000000003b7 <+871>:	movss  xmm0,DWORD PTR [rsi+rdi*4-0x4]
+   0x00000000000003bd <+877>:	divss  xmm0,xmm2
+   0x00000000000003c1 <+881>:	movss  DWORD PTR [r8+rdi*4-0x4],xmm0
 
-i = 0; i < arr_len; ++i) {
-<+2>:	test   rdx,rdx
-<+5>:	jbe    0x3c8 <floating_division_soa+888>
-<+14>:	xor    ecx,ecx
-<+38>:	mov    edi,0x1
-<+47>:	je     0x24d <floating_division_soa+509>
-<+74>:	inc    ecx
-<+491>:	cmp    rcx,rax
-<+494>:	jb     0x85 <floating_division_soa+53>
-<+500>:	shl    ecx,0x3
-<+503>:	movsxd rdi,ecx
-<+506>:	inc    rdi
-<+509>:	cmp    rdi,rdx
-<+512>:	ja     0x3c8 <floating_division_soa+888>
-<+526>:	jmp    QWORD PTR [rdx*8+0x0]
-
-results->r[i] = arr->r[i] / a;
-<+16>:	mov    r10,QWORD PTR [rsi]
-<+27>:	mov    r11,QWORD PTR [rdi]
-<+53>:	movsxd rdi,ecx
-<+59>:	shl    rdi,0x5
-<+76>:	movss  xmm3,DWORD PTR [r11+rdi*1]
-<+82>:	divss  xmm3,xmm0
-<+86>:	movss  DWORD PTR [r10+rdi*1],xmm3
-<+122>:	movss  xmm6,DWORD PTR [r11+rdi*1+0x4]
-<+129>:	divss  xmm6,xmm0
-<+133>:	movss  DWORD PTR [r10+rdi*1+0x4],xmm6
-<+176>:	movss  xmm9,DWORD PTR [r11+rdi*1+0x8]
-<+183>:	divss  xmm9,xmm0
-<+188>:	movss  DWORD PTR [r10+rdi*1+0x8],xmm9
-<+232>:	movss  xmm12,DWORD PTR [r11+rdi*1+0xc]
-<+239>:	divss  xmm12,xmm0
-<+244>:	movss  DWORD PTR [r10+rdi*1+0xc],xmm12
-<+288>:	movss  xmm15,DWORD PTR [r11+rdi*1+0x10]
-<+295>:	divss  xmm15,xmm0
-<+300>:	movss  DWORD PTR [r10+rdi*1+0x10],xmm15
-<+338>:	movss  xmm5,DWORD PTR [r11+rdi*1+0x14]
-<+345>:	divss  xmm5,xmm0
-<+349>:	movss  DWORD PTR [r10+rdi*1+0x14],xmm5
-<+387>:	movss  xmm8,DWORD PTR [r11+rdi*1+0x18]
-<+394>:	divss  xmm8,xmm0
-<+399>:	movss  DWORD PTR [r10+rdi*1+0x18],xmm8
-<+439>:	movss  xmm11,DWORD PTR [r11+rdi*1+0x1c]
-<+446>:	divss  xmm11,xmm0
-<+451>:	movss  DWORD PTR [r10+rdi*1+0x1c],xmm11
-<+533>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x14]
-<+543>:	divss  xmm3,xmm0
-<+547>:	movss  DWORD PTR [r10+rdi*4+0x14],xmm3
-<+585>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x10]
-<+595>:	divss  xmm3,xmm0
-<+599>:	movss  DWORD PTR [r10+rdi*4+0x10],xmm3
-<+637>:	movss  xmm3,DWORD PTR [r11+rdi*4+0xc]
-<+647>:	divss  xmm3,xmm0
-<+651>:	movss  DWORD PTR [r10+rdi*4+0xc],xmm3
-<+689>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x8]
-<+699>:	divss  xmm3,xmm0
-<+703>:	movss  DWORD PTR [r10+rdi*4+0x8],xmm3
-<+741>:	movss  xmm3,DWORD PTR [r11+rdi*4+0x4]
-<+751>:	divss  xmm3,xmm0
-<+755>:	movss  DWORD PTR [r10+rdi*4+0x4],xmm3
-<+793>:	movss  xmm3,DWORD PTR [r11+rdi*4]
-<+802>:	divss  xmm3,xmm0
-<+806>:	movss  DWORD PTR [r10+rdi*4],xmm3
-<+839>:	movss  xmm3,DWORD PTR [r11+rdi*4-0x4]
-<+846>:	divss  xmm3,xmm0
-<+850>:	movss  DWORD PTR [r10+rdi*4-0x4],xmm3
-
-results->g[i] = b / arr->g[i];
-<+19>:	mov    r14,QWORD PTR [rsi+0x8]
-<+30>:	mov    r9,QWORD PTR [rdi+0x8]
-<+56>:	movaps xmm4,xmm1
-<+63>:	movaps xmm7,xmm1
-<+66>:	movaps xmm10,xmm1
-<+70>:	movaps xmm13,xmm1
-<+92>:	movaps xmm3,xmm1
-<+95>:	divss  xmm4,DWORD PTR [r9+rdi*1]
-<+101>:	movss  DWORD PTR [r14+rdi*1],xmm4
-<+140>:	movaps xmm6,xmm1
-<+143>:	divss  xmm7,DWORD PTR [r9+rdi*1+0x4]
-<+150>:	movss  DWORD PTR [r14+rdi*1+0x4],xmm7
-<+195>:	movaps xmm9,xmm1
-<+199>:	divss  xmm10,DWORD PTR [r9+rdi*1+0x8]
-<+206>:	movss  DWORD PTR [r14+rdi*1+0x8],xmm10
-<+251>:	movaps xmm12,xmm1
-<+255>:	divss  xmm13,DWORD PTR [r9+rdi*1+0xc]
-<+262>:	movss  DWORD PTR [r14+rdi*1+0xc],xmm13
-<+307>:	divss  xmm3,DWORD PTR [r9+rdi*1+0x10]
-<+314>:	movss  DWORD PTR [r14+rdi*1+0x10],xmm3
-<+356>:	divss  xmm6,DWORD PTR [r9+rdi*1+0x14]
-<+363>:	movss  DWORD PTR [r14+rdi*1+0x14],xmm6
-<+406>:	divss  xmm9,DWORD PTR [r9+rdi*1+0x18]
-<+413>:	movss  DWORD PTR [r14+rdi*1+0x18],xmm9
-<+458>:	divss  xmm12,DWORD PTR [r9+rdi*1+0x1c]
-<+465>:	movss  DWORD PTR [r14+rdi*1+0x1c],xmm12
-<+540>:	movaps xmm4,xmm1
-<+554>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x14]
-<+561>:	movss  DWORD PTR [r14+rdi*4+0x14],xmm4
-<+592>:	movaps xmm4,xmm1
-<+606>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x10]
-<+613>:	movss  DWORD PTR [r14+rdi*4+0x10],xmm4
-<+644>:	movaps xmm4,xmm1
-<+658>:	divss  xmm4,DWORD PTR [r9+rdi*4+0xc]
-<+665>:	movss  DWORD PTR [r14+rdi*4+0xc],xmm4
-<+696>:	movaps xmm4,xmm1
-<+710>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x8]
-<+717>:	movss  DWORD PTR [r14+rdi*4+0x8],xmm4
-<+748>:	movaps xmm4,xmm1
-<+762>:	divss  xmm4,DWORD PTR [r9+rdi*4+0x4]
-<+769>:	movss  DWORD PTR [r14+rdi*4+0x4],xmm4
-<+799>:	movaps xmm4,xmm1
-<+812>:	divss  xmm4,DWORD PTR [r9+rdi*4]
-<+818>:	movss  DWORD PTR [r14+rdi*4],xmm4
-<+857>:	divss  xmm1,DWORD PTR [r9+rdi*4-0x4]
-<+864>:	movss  DWORD PTR [r14+rdi*4-0x4],xmm1
-
-results->b[i] = arr->b[i] / c;
-<+23>:	mov    r8,QWORD PTR [rsi+0x10]
-<+34>:	mov    rsi,QWORD PTR [rdi+0x10]
-<+107>:	movss  xmm5,DWORD PTR [rsi+rdi*1]
-<+112>:	divss  xmm5,xmm2
-<+116>:	movss  DWORD PTR [r8+rdi*1],xmm5
-<+157>:	movss  xmm8,DWORD PTR [rsi+rdi*1+0x4]
-<+164>:	divss  xmm8,xmm2
-<+169>:	movss  DWORD PTR [r8+rdi*1+0x4],xmm8
-<+213>:	movss  xmm11,DWORD PTR [rsi+rdi*1+0x8]
-<+220>:	divss  xmm11,xmm2
-<+225>:	movss  DWORD PTR [r8+rdi*1+0x8],xmm11
-<+269>:	movss  xmm14,DWORD PTR [rsi+rdi*1+0xc]
-<+276>:	divss  xmm14,xmm2
-<+281>:	movss  DWORD PTR [r8+rdi*1+0xc],xmm14
-<+321>:	movss  xmm4,DWORD PTR [rsi+rdi*1+0x10]
-<+327>:	divss  xmm4,xmm2
-<+331>:	movss  DWORD PTR [r8+rdi*1+0x10],xmm4
-<+370>:	movss  xmm7,DWORD PTR [rsi+rdi*1+0x14]
-<+376>:	divss  xmm7,xmm2
-<+380>:	movss  DWORD PTR [r8+rdi*1+0x14],xmm7
-<+420>:	movss  xmm10,DWORD PTR [rsi+rdi*1+0x18]
-<+427>:	divss  xmm10,xmm2
-<+432>:	movss  DWORD PTR [r8+rdi*1+0x18],xmm10
-<+472>:	movss  xmm13,DWORD PTR [rsi+rdi*1+0x1c]
-<+479>:	divss  xmm13,xmm2
-<+484>:	movss  DWORD PTR [r8+rdi*1+0x1c],xmm13
-<+568>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x14]
-<+574>:	divss  xmm5,xmm2
-<+578>:	movss  DWORD PTR [r8+rdi*4+0x14],xmm5
-<+620>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x10]
-<+626>:	divss  xmm5,xmm2
-<+630>:	movss  DWORD PTR [r8+rdi*4+0x10],xmm5
-<+672>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0xc]
-<+678>:	divss  xmm5,xmm2
-<+682>:	movss  DWORD PTR [r8+rdi*4+0xc],xmm5
-<+724>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x8]
-<+730>:	divss  xmm5,xmm2
-<+734>:	movss  DWORD PTR [r8+rdi*4+0x8],xmm5
-<+776>:	movss  xmm5,DWORD PTR [rsi+rdi*4+0x4]
-<+782>:	divss  xmm5,xmm2
-<+786>:	movss  DWORD PTR [r8+rdi*4+0x4],xmm5
-<+824>:	movss  xmm5,DWORD PTR [rsi+rdi*4]
-<+829>:	divss  xmm5,xmm2
-<+833>:	movss  DWORD PTR [r8+rdi*4],xmm5
-<+871>:	movss  xmm0,DWORD PTR [rsi+rdi*4-0x4]
-<+877>:	divss  xmm0,xmm2
-<+881>:	movss  DWORD PTR [r8+rdi*4-0x4],xmm0
-
-
+47	  }
 48	}
-<+888>:	pop    r14
-<+890>:	ret    
-<+891>:	nop    DWORD PTR [rax+rax*1+0x0]
+   0x00000000000003c8 <+888>:	pop    r14
+   0x00000000000003ca <+890>:	ret    
+   0x00000000000003cb <+891>:	nop    DWORD PTR [rax+rax*1+0x0]
 
-
+End of assembler dump.
