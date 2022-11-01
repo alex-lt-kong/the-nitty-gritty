e71f6530224f22bc1a81739223ee9fd05be01524
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-11-01 15:57:58 +0800

    update

--- c/01_memory-and-cpu-cache/07_cache-associativity/main.c
+++ c/01_memory-and-cpu-cache/07_cache-associativity/main.c
@@ -6,24 +6,21 @@
 
 int main() {
   srand(time(NULL));
-  
+  const size_t arr_len = 128 * 1024 * 1024 + 1;
+  uint32_t* arr = malloc(sizeof(uint32_t) * arr_len);
+
+  for (size_t i = 0; i < arr_len; ++i) {
+    arr[i] = rand();
+  }
   struct timespec ts;
   uint8_t sum = 0;
   double delta, t0;
-  size_t arr_len, idx;
-  const size_t repeats = 64 * 1024 * 1024;
-  uint32_t* arr;
-  for (size_t step = 1; step < 1024; ++step) {
-    arr_len = step * 1024 * 1024 / sizeof(uint32_t);
-    arr = malloc(sizeof(uint32_t) * arr_len);
-    for (size_t i = 0; i < arr_len; ++i) {
-        arr[i] = rand();
-    }
+  size_t idx;
+  for (size_t step = 1; step < 2049; ++step) {
     timespec_get(&ts, TIME_UTC);
     t0 = ts.tv_sec + ts.tv_nsec / 1000.0 / 1000.0 / 1000.0;
     idx = 0;
-
-    for (size_t i = 0; i < repeats; ++i) {        
+    for (size_t i = 0; i < arr_len; ++i) {        
         arr[idx] += step;
         sum += arr[idx];
         idx = (idx + step) % arr_len;
@@ -31,7 +28,8 @@ int main() {
     timespec_get(&ts, TIME_UTC);
     delta = ts.tv_sec + ts.tv_nsec / 1000.0 / 1000.0 / 1000.0 - t0;
     printf("%lu, %lf\n", step, delta);
-    free(arr);
   }
+
+  free(arr);
   return sum;
 }
