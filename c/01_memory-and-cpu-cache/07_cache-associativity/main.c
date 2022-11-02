072af7dd3f7fc501bbef878b10fad43755ff2f06
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-11-02 13:36:33 +0800

    update

--- c/01_memory-and-cpu-cache/07_cache-associativity/main.c
+++ c/01_memory-and-cpu-cache/07_cache-associativity/main.c
@@ -6,21 +6,21 @@
 
 int main() {
   srand(time(NULL));
-  const size_t arr_len = 128 * 1024 * 1024 + 1;
+  const uint64_t arr_len = 128 * 1024 * 1024 + 1;
   uint32_t* arr = malloc(sizeof(uint32_t) * arr_len);
 
-  for (size_t i = 0; i < arr_len; ++i) {
+  for (uint64_t i = 0; i < arr_len; ++i) {
     arr[i] = rand();
   }
   struct timespec ts;
   uint8_t sum = 0;
   double delta, t0;
-  size_t idx;
-  for (size_t step = 1; step < (8192) + 1; ++step) {
+  uint64_t idx;
+  for (uint64_t step = 1; step < (8192) + 1; ++step) {
     timespec_get(&ts, TIME_UTC);
     t0 = ts.tv_sec + ts.tv_nsec / 1000.0 / 1000.0 / 1000.0;
     idx = 0;
-    for (size_t i = 0; i < arr_len; ++i) {        
+    for (uint64_t i = 0; i < arr_len; ++i) {        
         arr[idx] += step;
         sum += arr[idx];
         idx = (idx + step) % arr_len;
--- c/01_memory-and-cpu-cache/07_cache-associativity/main_new.c
+++ c/01_memory-and-cpu-cache/07_cache-associativity/main_new.c
@@ -4,20 +4,20 @@
 #include <time.h>
 #include <string.h>
 
-#define CACHE_SIZE       (32*1024*1024L) // MUST be a power of 2
+#define CACHE_SIZE       (32*1024*1024UL) // MUST be a power of 2
 #define CACHE_LINE_SIZE  64              // MUST be a power of 2
-#define MAX_STEP         128            // MUST be a power of 2
+#define MAX_STEP         8192            // MUST be a power of 2
 #define ACCESSES         123435
 
 int main() {
-    const size_t arr_len = (CACHE_SIZE / CACHE_LINE_SIZE) * MAX_STEP;
+    const uint64_t arr_len = (CACHE_SIZE / CACHE_LINE_SIZE) * MAX_STEP;
     uint8_t* arr;
     struct timespec ts;
     uint8_t sum = 0;
     double delta, t0;
-    size_t idx;
-    size_t test_size;
-    size_t test_size_mask;
+    uint64_t idx;
+    uint64_t test_size;
+    uint64_t test_size_mask;
 
     arr = malloc(sizeof(uint8_t) * arr_len);   // Consider something like "mmap()" on Linux or "VirtualAlloc()" on Windows here
     if(arr == NULL) {
@@ -25,7 +25,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    for (size_t step = CACHE_LINE_SIZE; step <= MAX_STEP; step += 2) {
+    for (uint64_t step = CACHE_LINE_SIZE; step <= MAX_STEP; step += 2) {
 
         test_size = (CACHE_SIZE / CACHE_LINE_SIZE) * step;
         test_size_mask = test_size - 1;
@@ -38,12 +38,12 @@ int main() {
             sum += arr[idx];
             //idx = (idx + step) & test_size_mask;
             idx = (idx + step) % test_size;
-            if (((idx + step) & test_size_mask) == ((idx + step) % test_size)) {
-                printf(
-                    "%ld vs %ld\n",
-                    ((idx + step) & test_size_mask), ((idx + step) % test_size)
-                );
-            }
+           // if (((idx + step) & test_size_mask) != ((idx + step) % test_size)) {
+           //     printf(
+             //       "%lu vs %lu, test_size == %lu\n",
+                 //   ((idx + step) & test_size_mask), ((idx + step) % test_size), test_size
+               // );
+            //}
         }
 
         timespec_get(&ts, TIME_UTC);
