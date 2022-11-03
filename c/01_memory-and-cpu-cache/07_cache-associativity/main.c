d692e76339af9810db41caa067e2b393ee4354d0
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-11-03 16:10:46 +0800

    more on cache associativity

--- c/01_memory-and-cpu-cache/07_cache-associativity/main.c
+++ c/01_memory-and-cpu-cache/07_cache-associativity/main.c
@@ -6,7 +6,7 @@
 
 int main() {
   srand(time(NULL));
-  const uint64_t arr_len = 128 * 1024 * 1024 + 1;
+  const uint64_t arr_len = 32 * 1024 * 1024 + 1;
   uint32_t* arr = malloc(sizeof(uint32_t) * arr_len);
 
   for (uint64_t i = 0; i < arr_len; ++i) {
@@ -16,7 +16,7 @@ int main() {
   uint8_t sum = 0;
   double delta, t0;
   uint64_t idx;
-  for (uint64_t step = 1; step < (8192) + 1; ++step) {
+  for (uint64_t step = 1; step < (2048) + 1; ++step) {
     timespec_get(&ts, TIME_UTC);
     t0 = ts.tv_sec + ts.tv_nsec / 1000.0 / 1000.0 / 1000.0;
     idx = 0;
