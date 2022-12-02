314df60be8871a2379c5a9bf16aab2564151b1c6
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-12-02 11:30:22 +0800

    revise tz-aware dt handling

--- python/07_timezone-aware-dt/main.py
+++ python/07_timezone-aware-dt/main.py
@@ -12,7 +12,7 @@ def test(naive_datetime: dt.datetime) -> None:
         f'The navie_datetime is {naive_datetime}. '
         'Python is not aware of the timezone, but we know it is in UTC+8 (Asia/Hong_Kong).'
     )
-    # We can the Unix epoch for the given datetime object
+    # We can get the Unix epoch time for the given naive/timezone-agnostic datetime object
     unix_ts = pytz.timezone('Asia/Hong_Kong').localize(naive_datetime).timestamp()
 
     print(f'It is equivalent to unix timestamp: {unix_ts}')
@@ -30,3 +30,22 @@ print(
 )
 test(dt.datetime(1903, 1, 4, 15, 9, 26))
 print('Also note that this function seems to be unable to handle dates before 1970.')
+
+
+def incorrect_method(dt_str: str) -> None:
+    # If we are given a string of a specific timezone, we may be tempted to directly convert it into unix_datetime
+    # instead of using unix timestamp as a bridge.
+    # This would not be as straightforward.
+    naive_datetime = dt.datetime.strptime(dt_str, '%b %d %Y %I:%M:%S:%f%p')
+    unix_dt = pytz.timezone('US/Eastern').localize(naive_datetime)
+    # pytz.timezone('US/Eastern').localize(naive_datetime) produces a timezone-aware datetime
+    # if we just print(), if will still show the original datetime, with timezone info
+    print(unix_dt)
+    # >>> 2022-12-01 20:53:05-05:00
+    print(unix_dt.strftime('%Y-%m-%d %H:%M:%S'))
+    # >>> 2022-12-01 20:53:05
+    # This could be the more proper behavior after thinking it through, but this could be what we don't want:
+    # we may just want to convert a datetime string in one timezone to a datetime string to another.
+
+
+incorrect_method('Dec  1 2022  8:53:05:000PM')
