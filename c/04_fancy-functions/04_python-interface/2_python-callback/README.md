dff97a8f6a2065a69c5fe1cd55424a5f39f48a47
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-01-21 17:16:13 +0800

    minimal reproducible example

--- c/04_fancy-functions/04_python-interface/2_python-callback/README.md
+++ c/04_fancy-functions/04_python-interface/2_python-callback/README.md
@@ -1,1 +1,4 @@
 apt install python3.9-dev
+
+https://docs.python.org/3.3/library/ctypes.html?highlight=ctypes#callback-functions
+https://www.oreilly.com/library/view/python-cookbook/0596001673/ch16s07.html
--- c/04_fancy-functions/04_python-interface/2_python-callback/main.py
+++ c/04_fancy-functions/04_python-interface/2_python-callback/main.py
@@ -5,20 +5,23 @@ from numpy.ctypeslib import ndpointer
 so_file = "./func.so"
 funcs = CDLL(so_file)
 
-
-IntArray5 = c_int * 5
-ia = IntArray5(5, 1, 7, 33, 99)
+size = int(64)
+arr_np = np.random.randint(0, size, size, dtype=np.int32)  
+arr_c = arr_np
+#IntArray5 = c_int * 5
+#ia = IntArray5(5, 1, 7, 33, 99)
 qsort = funcs.qsort
 qsort.restype = None
 
 CMPFUNC = CFUNCTYPE(c_int, POINTER(c_int), POINTER(c_int))
+count = 0
 def py_cmp_func(a, b):
   print("py_cmp_func", a[0], b[0])
-  return a[0] > b[0]
+  return a[0] < b[0]
 
 cmp_func = CMPFUNC(py_cmp_func)
 
-funcs.qsort(ia, len(ia), sizeof(c_int), cmp_func) 
+funcs.qsort(arr_c.ctypes.data_as(POINTER(c_int)), size, sizeof(c_int), cmp_func) 
 
-for i in range(5):
-  print(ia[i])
+for i in range(size):
+  print(arr_c[i], end=', ')
