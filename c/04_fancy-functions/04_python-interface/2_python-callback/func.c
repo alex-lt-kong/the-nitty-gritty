72729f1a22565d3216517ce55360463e79e77e23
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-01-21 19:36:20 +0800

    add a C comparison

--- c/04_fancy-functions/04_python-interface/2_python-callback/func.c
+++ c/04_fancy-functions/04_python-interface/2_python-callback/func.c
@@ -1,7 +1,8 @@
 #include<stdlib.h>
 #include<stdint.h>
 
-void manipulate_inplace(uint64_t* arr, uint64_t arr_len, int64_t (*func)(int64_t)) {
+void manipulate_inplace(uint64_t* arr,
+    uint64_t arr_len, int64_t (*func)(int64_t)) {
     for (uint64_t i = 0; i < arr_len; ++i) {
         arr[i] = func(arr[i]);
     }
--- c/04_fancy-functions/04_python-interface/2_python-callback/main.py
+++ c/04_fancy-functions/04_python-interface/2_python-callback/main.py
@@ -2,10 +2,10 @@ from ctypes import *
 import numpy as np
 import time
 
-so_file = "./func.so"
-funcs = CDLL(so_file)
+so_path = "./func.so"
+so = CDLL(so_path)
 
-arr_size = 2_000_000_000
+arr_size = 2_000_000
 sample_size = 10;
 arr = np.random.randint(0, arr_size, arr_size, dtype=np.int64)
 sample_idxes = np.random.randint(0, arr_size, sample_size, dtype=np.int64)
@@ -14,15 +14,11 @@ for i in range(sample_size):
   print(arr[sample_idxes[i]], end=', ')
 print()
 
-CMPFUNC = CFUNCTYPE(c_int, c_int64)
-count = 0
 def apply(a):
   return a - 1
 
-apply_func = CMPFUNC(apply)
-
 start = time.time()
-funcs.manipulate_inplace(arr.ctypes.data_as(POINTER(c_int64)), arr_size, apply_func) 
+so.manipulate_inplace(arr.ctypes.data_as(POINTER(c_int64)), arr_size, CFUNCTYPE(c_int, c_int64)(apply)) 
 diff = time.time() - start
 
 print(f'{sample_size} samples become:', end='')
@@ -30,4 +26,4 @@ for i in range(sample_size):
   print(arr[sample_idxes[i]], end=', ')
 print()
 
-print(f'Calling back {arr_size/1_000_000:,.0f}mil times takes: {diff:.2f} sec ({arr_size/diff:,.0f} / sec)')
+print(f'Calling back {arr_size/1_000_000:,.0f}M times takes: {diff:.2f} sec ({arr_size/diff:,.0f} / sec)')
