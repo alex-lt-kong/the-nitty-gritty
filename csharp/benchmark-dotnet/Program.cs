5de28d69d3b6cfe1f79f77390dac4061964c3bbc
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-01-24 00:45:15 +0800

    a test on BenchmarkDotNet

---
+++ csharp/benchmark-dotnet/Program.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmarks
+{
+    
+    public class Iterations
+    {
+        UInt64 sum = 0;
+
+        [Benchmark]
+        public void IterateManyPlusOne() {
+            UInt64 sum = 0;
+            for (int i = 0; i < 1_000_000_000; ++i) {
+                for (int j = 0; j < 33; ++j) {
+                    ++sum;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void IterateOnePlusOne() {
+            UInt64 sum = 0;
+            for (int i = 0; i < 33; ++i) {
+                ++sum;
+            }
+        }
+
+        [Benchmark]
+        public void IterateMany() {
+            UInt64 sum = 0;
+            for (int i = 0; i < 1_000_000_000; ++i) {
+                for (int j = 0; j < 32; ++j) {
+                    ++sum;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void IterateOne() {
+            UInt64 sum = 0;
+            for (int i = 0; i < 32; ++i) {
+                ++sum;
+            }
+        }
+
+        public UInt64 GetSum() {
+            return sum;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary0 = BenchmarkRunner.Run<Iterations>();
+        }
+    }
+
+}
+
+
