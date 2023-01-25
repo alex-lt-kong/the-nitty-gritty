65a7cbf5e9bfef8b56b7bc5f7ca3bcac45fbb19c
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-01-25 19:03:47 +0800

    one more C# project

---
+++ csharp/compile-time-computing/compile-time-computing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyProgram
+{
+
+    public class Program
+    {
+        public static int Factorial(int n) {
+            int result = 1;
+            while (n > 1) {
+                result *= n--;
+            }
+            return result;
+        }
+
+        public static int GetConstantId() {
+            int id = 1 + 2 + 3 + 4 + 5;
+            return id;
+        }
+        public static void Main(string[] args)
+        {
+            int id = GetConstantId();
+            int f4 = Factorial(4);
+            Debugger.Break();
+            Console.WriteLine($"GetId(): {id}");
+            Console.WriteLine($"Factorial(): {f4}");
+        }
+    }
+
+}
+
+
