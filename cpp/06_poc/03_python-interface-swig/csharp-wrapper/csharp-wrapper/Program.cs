db35d1f0a447c39170bf21b62050be6abf1d816d
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-02-06 13:36:54 +0800

    make SWIG C# work

--- cpp/06_poc/03_python-interface-swig/csharp-wrapper/csharp-wrapper/Program.cs
+++ cpp/06_poc/03_python-interface-swig/csharp-wrapper/csharp-wrapper/Program.cs
@@ -2,13 +2,25 @@
 using System.ComponentModel;
 using System.Diagnostics;
 
+
 namespace MyProgram
 {
-    class NewIf : MyIf  // derived class (child)
+    class DepartmentA : Department
     {
-        public override void myfunc(Student stu)
-        {            
-            //Console.WriteLine("score1 is {0:0.00}", stu.score1);
+        public override void onStudentIterated(Student stu)
+        {
+        }
+    }
+    class DepartmentB : Department
+    {
+        UInt32 count = 0;
+        public override void onStudentIterated(Student stu)
+        {
+            ++count;
+            if (count % 10000 == 0)
+            {
+                Console.WriteLine($"{stu.score1},{stu.score2},{stu.score3},{stu.score4}"); 
+            }
         }
     }
     public class Program
@@ -17,10 +29,12 @@ namespace MyProgram
 
         public static void Main(string[] args)
         {
-            NewIf myIf = new NewIf();
-            UInt32 iter_count = 1000 * 1000 * 10;
+            DepartmentA dept = new DepartmentA();
+            UInt32 iter_count = 1000 * 1000;
+            DepartmentHandler deptHdl = new DepartmentHandler(dept, iter_count);
+            deptHdl.prepareStudentData();
             var start = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
-            myIf.start(iter_count);
+            deptHdl.start();
             var diff = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds() - start;
             Console.WriteLine($"{diff / 1000.0} sec, {1.0 * iter_count / diff * 1000} calls / sec");
         }
