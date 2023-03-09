e578c31c2e36764e6ccb32601056b8f8b303894c
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-03-09 10:13:33 +0800

    a more complete fork-exec

--- c-cpp/c/04_poc/06_subprocess/main.c
+++ c-cpp/c/04_poc/06_subprocess/main.c
@@ -12,16 +12,23 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    int pipefd_stdout[2], pipefd_stderr[2];
+    int pipefd_out[2], pipefd_err[2];
     // pipefd[0] is the read end of the pipe
     // pipefd[1] is the write end of the pipe
-    FILE* stdout;
-    FILE* stderr;
-    char line[256];
+    FILE* fp_out;
+    FILE* fp_err;
+    char buff[256];
     int status;
 
-    if (pipe(pipefd_stdout) == -1 || pipe(pipefd_stderr) == -1) {
+    if (pipe(pipefd_out) == -1) {
         // man 2 pipe
+        perror("pipe()");
+        return EXIT_FAILURE;
+    }
+    if (pipe(pipefd_err) == -1) {
+        // man 2 pipe
+        close(pipefd_out[0]);        
+        close(pipefd_out[0]);
         perror("pipe()");
         return EXIT_FAILURE;
     }
@@ -34,24 +41,25 @@ int main(int argc, char** argv) {
     }
 
     if (child_pid == 0) { // fork() succeeded, we are in the child process
-        close(pipefd_stdout[0]);
-        close(pipefd_stderr[0]);
+        close(pipefd_out[0]);
+        close(pipefd_err[0]);
         // man dup2
-        dup2(pipefd_stdout[1], STDOUT_FILENO);
-        dup2(pipefd_stderr[1], STDERR_FILENO);
+        dup2(pipefd_out[1], STDOUT_FILENO);
+        dup2(pipefd_err[1], STDERR_FILENO);
 
         // Prepared three possible cases, to demo different behaviors
         if (atoi(argv[1]) == 0) {
             execl("./sub.out", "./sub.out", (char*) NULL); 
         } else if (atoi(argv[1]) == 1) {
-            char *const parmList[] = {"/bin/ls", "-l", "/tmp/", NULL};
-            execv("/bin/ls", parmList);
+            char *const args[] = {"/bin/ls", "-l", "/tmp/", NULL};
+            execv(args[0], args);
         } else {
-            char *const parmList[] = {"/bin/ls", "-l", "/path/that/definitely/does/not/exist/", NULL};
-            execv("/bin/ls", parmList);
+            char *const args[] = {"/bin/ls", "-l",
+                "/path/that/definitely/does/not/exist/", NULL};
+            execv(args[0], args);
         }
 
-        perror("execl()");
+        perror("execl()/execv()");
         // The exec() functions return only if an error has occurred.
         // The return value is -1, and errno is set to indicate the error.
         exit(EXIT_FAILURE);
@@ -59,34 +67,49 @@ int main(int argc, char** argv) {
     }
     
     //Only parent gets here
-    close(pipefd_stdout[1]);
-    close(pipefd_stderr[1]);
+    close(pipefd_out[1]);
+    close(pipefd_err[1]);
 
-    if ((stdout = fdopen(pipefd_stdout[0], "r")) == NULL) {
+    if ((fp_out = fdopen(pipefd_out[0], "r")) == NULL) {
         perror("fdopen()");
         return EXIT_FAILURE;
     }
-    if ((stderr = fdopen(pipefd_stderr[0], "r")) == NULL) {
+    if ((fp_err = fdopen(pipefd_err[0], "r")) == NULL) {
         perror("fdopen()");
-        fclose(stdout);
+        fclose(fp_out);
         return EXIT_FAILURE;
     }
 
-    while(fgets(line, sizeof(line) - 1, stdout)) {            
-        printf("stdout: %s", line);
+    printf("===== stdout =====\n");
+    while(fgets(buff, sizeof(buff) - 1, fp_out)) {            
+        printf("%s", buff);
     }
-    while(fgets(line, sizeof(line) - 1, stderr)) {            
-        printf("stderr: %s", line);
-    }
-    fclose(stdout);
-    fclose(stderr);
+    printf("===== stdout =====\n");
+
+    printf("===== stderr =====\n");
+    while(fgets(buff, sizeof(buff) - 1, fp_err)) {            
+        printf("%s", buff);
+    }    
+    printf("===== stderr =====\n");
+    fclose(fp_out);
+    fclose(fp_err);
 
     // wait for the child process to terminate
-    waitpid(child_pid, &status, 0);
+    if (waitpid(child_pid, &status, 0) == -1) {
+        perror("waitpid()");
+        return EXIT_FAILURE;
+    }
     if (WIFEXITED(status)) {
         printf("Child process exited normally, rc: %d\n", WEXITSTATUS(status));
     } else {
-        printf("Child process exited abnormally.\n");
+        printf("Child process exited unexpectedly ");
+        if (WIFSIGNALED(status)) {
+            printf("(terminated by a signal: %d)\n", WTERMSIG(status));
+        } else if (WIFSTOPPED(status)) {
+            printf("(stopped by delivery of a signal: %d)\n", WSTOPSIG(status));
+        } else {
+            printf("(unknown status: %d)\n", status);
+        }
     }
     
 }
