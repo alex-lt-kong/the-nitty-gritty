026e5c377b5b21fcd3f378244d789f8ffbb119de
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-03-21 10:24:18 +0800

    update to shared memory

--- c-cpp/c/08_synchronization-and-ipc/2_shared-memory/1_hello-world/common.h
+++ c-cpp/c/08_synchronization-and-ipc/2_shared-memory/1_hello-world/common.h
@@ -1,4 +1,6 @@
 #define SHM_SIZE 512
+// man shm_open recommends to use a name starting with a slash and contains
+// no other slashes.
 #define SHM_NAME "/shm.me"
-#define SHM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 // o:wr, g:wr, i.e., 0660
+#define SHM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
