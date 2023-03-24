1f3d882522f80ed653e8accdf6cac2993e08c52b
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-03-24 12:59:18 +0800

    remove unnecessary components

--- mre/src/camera-server/deviceManager.h
+++ mre/src/camera-server/deviceManager.h
@@ -1,13 +1,9 @@
 #ifndef DEVICE_MANAGER_H
 #define DEVICE_MANAGER_H
 
-#include <linux/stat.h>
 #include <string>
 #include <pthread.h>
-#include <queue>
-#include <semaphore.h>
-#include <sys/time.h>
-#include <signal.h>
+
 
 #include "eventLoop.h"
 
