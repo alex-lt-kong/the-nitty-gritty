b19bc3f7aa1f19b774c44a82ceb75d5f8430a5c7
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-03-27 12:27:42 +0800

    more on signal handler

---
+++ c-cpp/common/04_posix-api/03_signal-handler/2_multithreading.c
@@ -0,0 +1,79 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <errno.h>
+
+volatile sig_atomic_t e_flag = 0;
+
+static void signal_handler(int signo) {
+    char msg[] = "Signal caught\n";
+    write(STDIN_FILENO, msg, strlen(msg));
+    e_flag = 1;
+}
+
+void* event_loop(void* param) {
+    int tid = *((int*)param);
+    size_t iter_count = 0;
+    while (e_flag == 0) {
+        ++ iter_count;
+        printf("Th %d: Event loop is running now, iterated %lu times ...\n",
+            tid, iter_count);
+        for (size_t i = 0; i < tid + 1 && e_flag == 0; ++ i) {
+            sleep(1);
+        }
+    }
+    size_t* ret = (size_t*)malloc(sizeof(int));
+    *ret = iter_count;
+    return ret;
+}
+
+int main() {
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        perror("signal()");
+        return EXIT_FAILURE;
+    }
+    printf("A signal handler is installed, "
+        "press Ctrl+C to exit event loop threads gracefully.\n");
+
+    pthread_t threads[8];
+    size_t running_thread_count = 0;
+    int tids[sizeof(threads) / sizeof(threads[0])];
+    for (size_t i = 0; i < sizeof(threads) / sizeof(threads[0]); ++i) {
+        tids[i] = i;
+        int err_no;
+        // pthread_create() doesn't set errno,
+        // we need to catch its retval manually
+        if ((err_no = pthread_create(
+            &threads[i], NULL, event_loop, (void *)&tids[i])) != 0) {
+            fprintf(stderr, "pthread_create() failed: %d(%s), "
+                "the program exits now", err_no, strerror(err_no));
+            if ((err_no = raise(SIGINT)) != 0) {
+                fprintf(stderr, "even raise() failed: %d(%s), "
+                    "the program exits UNgracefully",
+                    err_no, strerror(err_no));
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
+        ++running_thread_count;
+    }
+
+    for (size_t i = 0; i < running_thread_count; ++i) {
+        size_t* ret;
+        int err_no;
+        // pthread_join() doesn't set errno,
+        // we need to catch its retval manually
+        if ((err_no = pthread_join(threads[i], (void**)&ret)) != 0) {
+            fprintf(stderr, "pthread_join() failed: %d(%s), "
+                "but there is nothing much we can do",
+                err_no, strerror(err_no));
+        } else {
+            printf("Th %lu returned, iterated: %lu times\n", i, *ret);
+            free(ret);
+        }
+    }
+    return EXIT_SUCCESS;
+}
