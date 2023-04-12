b29fd5f6466d085008b85f0d1ff2cf6944857f34
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-04-12 18:29:42 +0800

    test AFL++

---
+++ c-cpp/common/01_debugging-tools/aflpp/main.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_sum(int num, int depth, int max_depth) {
+	if (depth < max_depth) {
+		return get_sum(num + depth, depth+1, max_depth);
+	} else {
+		return num + depth;
+	}
+}
+
+int main(void) {
+    int len, idx;
+    scanf("%d %d", &len, &idx);
+    int* arr = (int*)malloc(len * sizeof(int));
+    arr[idx] = idx;
+    arr[idx+1] = len;
+    printf("sum=%d\n", arr[idx] + arr[idx+1]);
+    printf("quotient=%d\n", arr[idx] / arr[idx+1]);
+	printf("recursive sum=%d\n", get_sum(arr[idx], 0, arr[idx+1]));
+    free(arr);
+    return 0;
+}
