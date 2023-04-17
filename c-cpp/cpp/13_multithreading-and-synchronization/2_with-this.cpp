2798fa2f0cac0ef83e5dd37436f8144e05b025b3
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-04-17 13:37:43 +0800

    multithreading and cv done

--- c-cpp/cpp/13_multithreading-and-synchronization/2_with-this.cpp
+++ c-cpp/cpp/13_multithreading-and-synchronization/2_with-this.cpp
@@ -6,9 +6,16 @@
 #include <unistd.h>
 #include <atomic>
 
+#include "common.h"
+
 using namespace std;
 
 static size_t num_objects = 10;
+/* While bool might be implemented in an atomic manner on x86/ARM/etc, 
+this is not guaranteed by C++ standard. To play it safe and to be
+more standard-compliant, we may want to use the more proper atomic<bool>
+type*/
+atomic<bool> should_stop;
 
 class MyClass {
 
@@ -22,68 +29,27 @@ public:
     }
     ~MyClass() {}
     void start() {
-        running = true;
         th = thread(&MyClass::event_loop, this);
     }
     void event_loop() {        
-        while (running) {
+        while (!should_stop) {
             this_thread::sleep_for(chrono::milliseconds(1000));
             cout << "[" << thread_id << "] iterating..." << endl; 
         }
         cout << "[" << thread_id << "] event_loop() exited gracefully" << endl;
     }
     void wait() {
+        // joinable() is a must--sometimes a thread already quits when we want 
+        // to join(), causing unexpected behavior
         if (th.joinable()) {
             th.join();
         }
     }
-    void stop() {
-        running = false;
-    }
 
 private:
     thread th;
-    /* While bool might be implemented in an atomic manner on x86/ARM/etc, 
-    this is not guaranteed by C++ standard. To play it safe and to be
-    more standard-compliant, we may want to use the more proper atomic<bool>
-    type*/
-    atomic<bool> running;
 };
 vector<MyClass> objs;
-
-static void signal_handler(int signum) {
-    char msg[] = "Signal [  ] caught\n";
-    msg[8] = '0' + signum / 10;
-    msg[9] = '0' + signum % 10;
-    size_t len = sizeof(msg) - 1;
-    size_t written = 0;
-    while (written < len) {
-        ssize_t ret = write(STDOUT_FILENO, msg + written, len - written);
-        if (ret == -1) {
-            perror("write()");
-            break;
-        }
-        written += ret;
-    }
-    for (int i = 0; i < num_objects; ++i) {
-        objs[i].stop();
-    }
-}
-
-void install_signal_handler() {
-    struct sigaction act;
-    if (sigemptyset(&act.sa_mask) == -1) {
-        perror("sigemptyset()");
-        abort();
-    }
-    act.sa_handler = signal_handler;  
-    act.sa_flags = 0;
-    if (sigaction(SIGINT,  &act, 0) + sigaction(SIGABRT, &act, 0) +
-        sigaction(SIGQUIT, &act, 0) + sigaction(SIGTERM, &act, 0) < 0) {
-        perror("sigaction()");
-        abort();
-    }
-}
 
 int main() {
 
--- c-cpp/cpp/13_multithreading-and-synchronization/4_with-cv.cpp
+++ c-cpp/cpp/13_multithreading-and-synchronization/4_with-cv.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <signal.h>
 #include <unistd.h>
 #include <atomic>
 #include <mutex>
 #include <queue>
 #include <condition_variable>
+
+#include "common.h"
 
 using namespace std;
 
@@ -18,15 +19,29 @@ condition_variable cv;
 queue<string> msg_queue;
 mutex stdout_mutex;
 
+/* While bool might be implemented in an atomic manner on x86/ARM/etc, 
+this is not guaranteed by C++ standard. To play it safe and to be
+more standard-compliant, we may want to use the more proper atomic<bool>
+type*/
+atomic<bool> should_stop;
+
+
 void stdout_writer() {
-    while (true) {
+    while (!should_stop) {
         unique_lock<mutex> lk(stdout_mutex);
-        cv.wait(lk, []{return !msg_queue.empty();});
+        /* The 2nd parameter, a predicate which returns ​false if the waiting
+        should be continued (bool(stop_waiting()) == false). The signature of
+        the predicate function should be equivalent to the following:
+        bool pred();​
+        cv.wait() implicitly calls lk.unlock() at the moment it blocks this
+        thread, this is also why it won't cause a deadlock when it is wait()ing
+        */
+        cv.wait(lk, []{ return !msg_queue.empty(); });
         string msg = msg_queue.front();
         msg_queue.pop();
-        lk.unlock();
         cout << msg << endl;
     }
+    cout << "[stdout_writer()] exited gracefully" << endl;
 }
 
 class MyClass {
@@ -41,18 +56,24 @@ public:
     }
     ~MyClass() {}
     void start() {
-        running = true;
         th = thread(&MyClass::event_loop, this);
     }
-    void event_loop() {        
-        while (running) {
+    void event_loop() {
+        size_t count = 0;
+        while (!should_stop) {
             this_thread::sleep_for(chrono::milliseconds(1000));
-            unique_lock<mutex> lk(stdout_mutex);
-            string msg = "[" + to_string(thread_id) + "] iterating...";
-            cv.wait(lk);
-            msg_queue.push(msg);
             // As lk's scope is this iteration, it will automatically unlock()
-            // when it goes out of scope.
+            // when it goes out of scope. This follows the paradigm of RAII
+            string msg = "[" + to_string(thread_id) + "] iterating... " +
+                to_string(count++);
+            unique_lock<mutex> lk(stdout_mutex);
+            msg_queue.push(msg);
+            lk.unlock();
+            /* There is an alternative method, cv.notiy_all(), but given
+            that there is only one waiting thread, these two are equivalent.*/
+            cv.notify_one();
+            this_thread::sleep_for(chrono::milliseconds(thread_id * count));
+
         }
         cout << "[" << thread_id << "] event_loop() exited gracefully" << endl;
     }
@@ -61,56 +82,14 @@ public:
             th.join();
         }
     }
-    void stop() {
-        running = false;
-    }
 
 private:
     thread th;
-    /* While bool might be implemented in an atomic manner on x86/ARM/etc, 
-    this is not guaranteed by C++ standard. To play it safe and to be
-    more standard-compliant, we may want to use the more proper atomic<bool>
-    type*/
-    atomic<bool> running;
 };
 vector<MyClass> objs;
 
-static void signal_handler(int signum) {
-    char msg[] = "Signal [  ] caught\n";
-    msg[8] = '0' + signum / 10;
-    msg[9] = '0' + signum % 10;
-    size_t len = sizeof(msg) - 1;
-    size_t written = 0;
-    while (written < len) {
-        ssize_t ret = write(STDOUT_FILENO, msg + written, len - written);
-        if (ret == -1) {
-            perror("write()");
-            break;
-        }
-        written += ret;
-    }
-    for (int i = 0; i < num_objects; ++i) {
-        objs[i].stop();
-    }
-}
-
-void install_signal_handler() {
-    struct sigaction act;
-    if (sigemptyset(&act.sa_mask) == -1) {
-        perror("sigemptyset()");
-        abort();
-    }
-    act.sa_handler = signal_handler;  
-    act.sa_flags = 0;
-    if (sigaction(SIGINT,  &act, 0) + sigaction(SIGABRT, &act, 0) +
-        sigaction(SIGQUIT, &act, 0) + sigaction(SIGTERM, &act, 0) < 0) {
-        perror("sigaction()");
-        abort();
-    }
-}
-
 int main() {
-
+    should_stop = false;
     for (int i = 0; i < num_objects; ++i) {
         objs.push_back(MyClass(i));
     }
@@ -118,6 +97,15 @@ int main() {
     cout << "Launched from the main\n";
 
     thread th = thread(&stdout_writer);
+    /* Per C++ Standard Committee's design, a thread must be either join()ed
+    or detach()ed, even if the exeuction already quits at the time of
+    join()ing or detach()ing; otherwise it causes "terminate called
+    without an active exception" then SIGABRT: 
+    https://stackoverflow.com/questions/7381757/c-terminate-called-without-an-active-exception
+    In C program, while I haven't seen this problem, some similar issues
+    seem to show the same behavior.
+    */
+    th.detach();
 
     for (int i = 0; i < num_objects; ++i) {
         objs[i].start();
@@ -126,6 +114,5 @@ int main() {
     for (int i = 0; i < num_objects; ++i) {
         objs[i].wait();
     }
-
     return 0;
 }
