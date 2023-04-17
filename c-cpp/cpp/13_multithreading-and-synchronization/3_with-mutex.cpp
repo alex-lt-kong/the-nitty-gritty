a365aed7300aecd43c78098ab28532844635616b
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-04-17 13:37:43 +0800

    multithreading and cv done

--- c-cpp/cpp/13_multithreading-and-synchronization/3_with-mutex.cpp
+++ c-cpp/cpp/13_multithreading-and-synchronization/3_with-mutex.cpp
@@ -7,10 +7,17 @@
 #include <atomic>
 #include <mutex>
 
+#include "common.h"
+
 using namespace std;
 
 static size_t num_objects = 10;
 mutex stdout_mutex;
+/* While bool might be implemented in an atomic manner on x86/ARM/etc, 
+this is not guaranteed by C++ standard. To play it safe and to be
+more standard-compliant, we may want to use the more proper atomic<bool>
+type*/
+atomic<bool> should_stop;
 
 class MyClass {
 
@@ -24,71 +31,31 @@ public:
     }
     ~MyClass() {}
     void start() {
-        running = true;
         th = thread(&MyClass::event_loop, this);
     }
     void event_loop() {        
-        while (running) {
+        while (!should_stop) {
             this_thread::sleep_for(chrono::milliseconds(1000));
             unique_lock<mutex> lk(stdout_mutex);
-            cout << "[" << thread_id << "] iterating..." << endl;
             // As lk's scope is this iteration, it will automatically unlock()
-            // when it goes out of scope.
+            // when it goes out of scope. This implements the RAII pattern.
+            cout << "[" << thread_id << "] iterating..." << endl;
         }
         cout << "[" << thread_id << "] event_loop() exited gracefully" << endl;
     }
     void wait() {
+        // joinable() is a must--sometimes a thread already quits when we want 
+        // to join(), causing unexpected behavior
         if (th.joinable()) {
             th.join();
         }
     }
-    void stop() {
-        running = false;
-    }
 
 private:
     thread th;
-    /* While bool might be implemented in an atomic manner on x86/ARM/etc, 
-    this is not guaranteed by C++ standard. To play it safe and to be
-    more standard-compliant, we may want to use the more proper atomic<bool>
-    type*/
-    atomic<bool> running;
 };
 vector<MyClass> objs;
 
-static void signal_handler(int signum) {
-    char msg[] = "Signal [  ] caught\n";
-    msg[8] = '0' + signum / 10;
-    msg[9] = '0' + signum % 10;
-    size_t len = sizeof(msg) - 1;
-    size_t written = 0;
-    while (written < len) {
-        ssize_t ret = write(STDOUT_FILENO, msg + written, len - written);
-        if (ret == -1) {
-            perror("write()");
-            break;
-        }
-        written += ret;
-    }
-    for (int i = 0; i < num_objects; ++i) {
-        objs[i].stop();
-    }
-}
-
-void install_signal_handler() {
-    struct sigaction act;
-    if (sigemptyset(&act.sa_mask) == -1) {
-        perror("sigemptyset()");
-        abort();
-    }
-    act.sa_handler = signal_handler;  
-    act.sa_flags = 0;
-    if (sigaction(SIGINT,  &act, 0) + sigaction(SIGABRT, &act, 0) +
-        sigaction(SIGQUIT, &act, 0) + sigaction(SIGTERM, &act, 0) < 0) {
-        perror("sigaction()");
-        abort();
-    }
-}
 
 int main() {
 
---
+++ c-cpp/cpp/13_multithreading-and-synchronization/common.h
@@ -0,0 +1,40 @@
+#include <atomic>
+#include <signal.h>
+
+extern std::atomic<bool> should_stop;
+
+void stop_all_threads() {
+    should_stop = true;
+}
+
+static void signal_handler(int signum) {
+    char msg[] = "Signal [  ] caught\n";
+    msg[8] = '0' + signum / 10;
+    msg[9] = '0' + signum % 10;
+    size_t len = sizeof(msg) - 1;
+    size_t written = 0;
+    while (written < len) {
+        ssize_t ret = write(STDOUT_FILENO, msg + written, len - written);
+        if (ret == -1) {
+            perror("write()");
+            break;
+        }
+        written += ret;
+    }
+    stop_all_threads();
+}
+
+void install_signal_handler() {
+    struct sigaction act;
+    if (sigemptyset(&act.sa_mask) == -1) {
+        perror("sigemptyset()");
+        abort();
+    }
+    act.sa_handler = signal_handler;  
+    act.sa_flags = 0;
+    if (sigaction(SIGINT,  &act, 0) + sigaction(SIGABRT, &act, 0) +
+        sigaction(SIGQUIT, &act, 0) + sigaction(SIGTERM, &act, 0) < 0) {
+        perror("sigaction()");
+        abort();
+    }
+}
