d8a58b9e73514ee3cc61462a0ac518c0718d8c87
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-07-31 10:38:52 +0800

    mat diff check

--- c-cpp/common/05_parallelism/3_cuda/2_gemm/openblas.cpp
+++ c-cpp/common/05_parallelism/3_cuda/2_gemm/openblas.cpp
@@ -9,7 +9,7 @@ using dtype = float;
 int main(void) {
   blasint m = 30000;
   blasint k = 8000;
-  blasint n = 11000;
+  blasint n = 1100;
   const blasint lda = m;
   const blasint ldb = k;
   const blasint ldc = m;
@@ -17,13 +17,10 @@ int main(void) {
   const dtype beta = 0.0;
   std::cout << "Reading A..." << std::endl;
   std::vector<dtype> A = readVector<dtype>("./a.in", m * k);
-  std::cout << "Done\nReading B... " << std::endl;
+  std::cout << "Done (" << A.size() << ")\nReading B... " << std::endl;
   std::vector<dtype> B = readVector<dtype>("./b.in", k * n);
-  std::cout << "Done" << std::endl;
+  std::cout << "Done (" << B.size() << ")" << std::endl;
   std::vector<dtype> C(m * n);
-
-  std::cout << A.size() << std::endl;
-  std::cout << B.size() << std::endl;
 
   printf("A\n");
   print_matrix(m, k, A.data(), lda);
@@ -31,15 +28,19 @@ int main(void) {
 
   printf("B\n");
   print_matrix(k, n, B.data(), ldb);
-  std::cout << "=====\nWriting C...\n";
-  write_matrix_to_csv(C, m, n, "./openblas.csv.out");
+  std::cout << "=====\n";
+
   std::cout << "Done" << std::endl;
   uint64_t t0 = get_timestamp_in_microsec();
   /* When throwing error, the argument count starts from 0*/
   cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha,
               A.data(), lda, B.data(), ldb, beta, C.data(), ldc);
   uint64_t t1 = get_timestamp_in_microsec();
+
   print_matrix(m, n, C.data(), m);
+  std::cout << "=====\nWriting C...\n";
+  write_matrix_to_csv(C, m, n, "./openblas.csv.out");
+  std::cout << "Done" << std::endl;
   std::cout << "Total: " << (t1 - t0) / 1000.0 << "ms" << std::endl;
   return 0;
 }
