fa0527c75d0da64ee07eaa5d70f4a542bde2ccdf
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-08-21 23:54:47 +0800

    benchmark taking shape

--- c-cpp/cpp/06_poc/05_cudacodec-vs-ffmpeg/cudacodec.cpp
+++ c-cpp/cpp/06_poc/05_cudacodec-vs-ffmpeg/cudacodec.cpp
@@ -1,4 +1,5 @@
 
+#include <chrono>
 #include <iostream>
 #include <string>
 
@@ -6,44 +7,55 @@
 #include <opencv2/cudacodec.hpp>
 #include <opencv2/highgui.hpp>
 
-#include "utils.h"
+#include "utils.hpp"
+
+using namespace std;
 
 int main(int argc, const char *argv[]) {
-  std::cout << cv::getBuildInformation() << std::endl;
+  cout << cv::getBuildInformation() << endl;
   if (argc != 3) {
-    std::cerr << "Usage : " << argv[0] << "  <Source URI> <Dest path>"
-              << std::endl;
+    cerr << "Usage : " << argv[0] << "  <Source URI> <Dest path>" << endl;
     return -1;
   }
 
   install_signal_handler();
-  std::cout << "A signal handler is installed, "
-               "press Ctrl+C to exit the event loop gracefully."
-            << std::endl;
+  cout << "A signal handler is installed, "
+          "press Ctrl+C to exit the event loop gracefully."
+       << endl;
 
-  cv::cuda::GpuMat dFrame;
+  cv::cuda::GpuMat dFrameCurr, dFramePrev;
 
   cv::Ptr<cv::cudacodec::VideoReader> dReader =
-      cv::cudacodec::createVideoReader(std::string(argv[1]));
+      cv::cudacodec::createVideoReader(string(argv[1]));
   cv::Ptr<cv::cudacodec::VideoWriter> dWriter =
-      cv::cudacodec::createVideoWriter(
-          std::string(argv[2]), cv::Size(1280, 720), cv::cudacodec::Codec::H264,
-          25.0, cv::cudacodec::ColorFormat::BGRA);
+      cv::cudacodec::createVideoWriter(string(argv[2]), cv::Size(1280, 720),
+                                       cv::cudacodec::Codec::H264, 25.0,
+                                       cv::cudacodec::ColorFormat::BGRA);
   size_t frameCount = 0;
   while (!e_flag) {
-
-    if (!dReader->nextFrame(dFrame)) {
-      std::cerr << "dReader->nextFrame(dFrame) is False" << std::endl;
+    if (!dFrameCurr.empty()) {
+      dFramePrev = dFrameCurr.clone();
+    }
+    if (!dReader->nextFrame(dFrameCurr)) {
+      cerr << "dReader->nextFrame(dFrame) is False" << endl;
       break;
     }
-
-    std::cout << "frameCount: " << ++frameCount << ", size(): " << dFrame.size()
-              << ", channels(): " << dFrame.channels() << std::endl;
-    if (!dFrame.empty()) {
-      dWriter->write(dFrame);
+    ++frameCount;
+    auto start = chrono::high_resolution_clock::now();
+    float diff = getCudaFrameChanges(dFramePrev, dFrameCurr);
+    auto end = chrono::high_resolution_clock::now();
+    if (!dFramePrev.empty() && frameCount % 10 == 0) {
+      cout << "frameCount: " << frameCount << ", size(): " << dFrameCurr.size()
+           << ", channels(): " << dFrameCurr.channels() << ", diff: " << diff
+           << "("
+           << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+           << " ms)" << endl;
+    }
+    if (!dFrameCurr.empty()) {
+      dWriter->write(dFrameCurr);
     }
   }
   dWriter->release();
-  std::cout << "dWriter->release()ed" << std::endl;
+  cout << "dWriter->release()ed" << endl;
   return 0;
 }
--- c-cpp/cpp/06_poc/05_cudacodec-vs-ffmpeg/ffmpeg.cpp
+++ c-cpp/cpp/06_poc/05_cudacodec-vs-ffmpeg/ffmpeg.cpp
@@ -1,4 +1,5 @@
 
+#include <chrono>
 #include <iostream>
 #include <string>
 
@@ -6,49 +7,61 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 
-#include "utils.h"
+#include "utils.hpp"
+
+using namespace std;
 
 int main(int argc, const char *argv[]) {
-  std::cout << cv::getBuildInformation() << std::endl;
+  cout << cv::getBuildInformation() << endl;
   if (argc != 3) {
-    std::cerr << "Usage : " << argv[0] << "  <Source URI> <Dest path>"
-              << std::endl;
+    cerr << "Usage : " << argv[0] << "  <Source URI> <Dest path>" << endl;
     return -1;
   }
 
   install_signal_handler();
-  std::cout << "A signal handler is installed, "
-               "press Ctrl+C to exit the event loop gracefully."
-            << std::endl;
+  cout << "A signal handler is installed, "
+          "press Ctrl+C to exit the event loop gracefully."
+       << endl;
 
   cv::VideoCapture cap;
-  bool result = cap.open(std::string(argv[1]), cv::CAP_FFMPEG,
-  {cv::CAP_PROP_HW_ACCELERATION, cv::VIDEO_ACCELERATION_ANY});
+  bool result =
+      cap.open(string(argv[1]), cv::CAP_ANY,
+               {cv::CAP_PROP_HW_ACCELERATION, cv::VIDEO_ACCELERATION_ANY});
   if (!result) {
-    std::cerr << "Error!" << std::endl;
+    cerr << "Error!" << endl;
   }
   cv::VideoWriter vwriter = cv::VideoWriter(
-      std::string(argv[2]), cv::CAP_ANY,
-      cv::VideoWriter::fourcc('a', 'v', 'c', '1'),
-      25.0, cv::Size(1280, 720)  ,
+      string(argv[2]), cv::CAP_ANY, cv::VideoWriter::fourcc('m', 'p', '4', 'v'),
+      25.0, cv::Size(1280, 720),
       {cv::VIDEOWRITER_PROP_HW_ACCELERATION, cv::VIDEO_ACCELERATION_ANY});
 
-  cv::Mat hFrame;
+  cv::Mat hFrameCurr, hFramePrev;
   size_t frameCount = 0;
   while (!e_flag) {
 
-    if (!cap.read(hFrame)) {
-      std::cerr << "cap.read(hFrame) is False" << std::endl;
+    if (!hFrameCurr.empty()) {
+      hFramePrev = hFrameCurr.clone();
+    }
+    if (!cap.read(hFrameCurr)) {
+      cerr << "cap.read(hFrame) is False" << endl;
       break;
     }
-
-    std::cout << "frameCount: " << ++frameCount << ", size(): " << hFrame.size()
-              << ", channels(): " << hFrame.channels() << std::endl;
-    if (!hFrame.empty()) {
-      vwriter.write(hFrame);
+    ++frameCount;
+    auto start = chrono::high_resolution_clock::now();
+    float diff = getFrameChanges(hFramePrev, hFrameCurr);
+    auto end = chrono::high_resolution_clock::now();
+    if (!hFramePrev.empty() && frameCount % 10 == 0) {
+      cout << "frameCount: " << frameCount << ", size(): " << hFrameCurr.size()
+           << ", channels(): " << hFrameCurr.channels() << ", diff: " << diff
+           << "("
+           << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+           << " ms)" << endl;
+    }
+    if (!hFrameCurr.empty()) {
+      vwriter.write(hFrameCurr);
     }
   }
   vwriter.release();
-  std::cout << "vwriter.release()ed" << std::endl;
+  cout << "vwriter.release()ed" << endl;
   return 0;
 }
