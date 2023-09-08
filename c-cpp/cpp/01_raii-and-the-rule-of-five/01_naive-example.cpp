09b09a0e05de7bdb54c0d4bae50e8caa1d8a6df6
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-09-08 23:40:21 +0800

    reorganize project and add exception safety

---
+++ c-cpp/cpp/01_raii-and-the-rule-of-five/01_naive-example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class DatabaseHelper {
+public:
+  DatabaseHelper() { cout << "DatabaseHelper() called" << endl; }
+  ~DatabaseHelper() { cout << "~DatabaseHelper() called" << endl; }
+  void aFancyMethod() { cout << "aFancyMethod() called" << endl; }
+
+  void anInvalidMethod() {
+    cout << "anInvalidMethod() called, exception will be thrown" << endl;
+    throw "A snake oil exception!";
+  }
+};
+
+int main() {
+  try {
+    DatabaseHelper myHelper = DatabaseHelper();
+    myHelper.aFancyMethod();
+    myHelper.anInvalidMethod();
+  } catch (const char *msg) {
+    cout << "A char exception caught, its content is: " << msg << endl;
+  }
+  cout << "Fear not, ~DatabaseHelper() will be called anyway!" << endl;
+  return 0;
+}
