f2b85198c440f7eb9604cc963f2853891bcc3efb
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-12-07 19:15:35 +0800

    more on std::forward

--- c-cpp/cpp-only/11_mini-features/05_move/main.cpp
+++ c-cpp/cpp-only/11_mini-features/05_move/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+
 void notMoving(std::string &&rhs) {
   std::string s = rhs;
   std::cout << "s@notMoving(): " << s << std::endl;
@@ -9,12 +10,43 @@ void moving(std::string &&rhs) {
   std::cout << "s@moving(): " << s << std::endl;
 }
 
-int main(void) {
+void test1_MoveMayNotMoveAnything() {
+  std::cout << "test1_MoveMayNotMoveAnything:\n";
   std::string s = "Hello world!";
   std::cout << "s is: " << s << std::endl;
   notMoving(std::move(s));
   std::cout << "s is: " << s << std::endl;
   moving(std::move(s));
   std::cout << "s is: " << s << std::endl;
+}
+
+void overloaded(int const &arg) { std::cout << "by lvalue\n"; }
+void overloaded(int &&arg) { std::cout << "by rvalue\n"; }
+
+template <typename t> void forwarding(t &&arg) {
+  std::cout << "via std::forward: ";
+  overloaded(std::forward<t>(arg));
+  std::cout << "via std::move: ";
+  overloaded(std::move(arg)); // conceptually this would invalidate arg
+  std::cout << "by simple passing: ";
+  overloaded(arg);
+}
+
+void test2_MoveVsForward() {
+  std::cout << "test2_MoveVsForward:\n";
+  std::cout << "--- initial caller passes rvalue ---\n";
+  forwarding(5);
+  std::cout << "--- initial caller passes lvalue ---\n";
+  int x = 5;
+  // forwarding(x) only works if forwarding() is a function template instead of
+  // a concrete function.
+  forwarding(x);
+}
+
+int main(void) {
+  test1_MoveMayNotMoveAnything();
+  std::cout << "\n";
+  test2_MoveVsForward();
+  std::cout << "\n";
   return 0;
 }
