e11938764ba51bc0db2c0501896595316393f741
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-01-06 01:17:02 +0800

    expand the PoC

---
+++ csharp/compilation-on-the-fly/test01_static-loading/Program.cs
@@ -0,0 +1,8 @@
+﻿// The mylib.dll is generated from main.cs@RuntimeCompiler
+
+using UtilityLibraries;
+
+Console.WriteLine(NumberLibrary.IsEven(14));
+Console.WriteLine(NumberLibrary.IsEven(2147483647));
+
+
---
+++ csharp/compilation-on-the-fly/test03_implement_interface/IDemoInterface.cs
@@ -0,0 +1,4 @@
+public interface IDemoInterface
+{
+    int DemoMethod(int value);
+}
