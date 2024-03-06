a169a5765fd1fdb7f502059e4d5347906715404f
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-03-06 12:14:25 +0800

    separate PoCs

--- csharp/compilation-on-the-fly/test01_static-loading/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// The mylib.dll is generated from main.cs@RuntimeCompiler
-
-using UtilityLibraries;
-
-Console.WriteLine(NumberLibrary.IsEven(14));
-Console.WriteLine(NumberLibrary.IsEven(2147483647));
-
-
