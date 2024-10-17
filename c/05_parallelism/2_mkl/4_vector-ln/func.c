50278ce178d7e2470e1321bd007b5dc547037821
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-10-17 20:26:51 +0800

    more update...

--- c/05_parallelism/2_mkl/4_vector-ln/func.c
+++ c/05_parallelism/2_mkl/4_vector-ln/func.c
@@ -5,9 +5,14 @@
 #include <math.h>
 #include <stdint.h>
 
+// Somehow this is needed on Linux...
+extern inline uint64_t get_timestamp_in_microsec();
 
-__declspec(dllexport)  double mkl_ln(uint64_t arr_size, double *restrict vec_in,
-              double *restrict vec_out) {
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+    double mkl_ln(uint64_t arr_size, double *restrict vec_in,
+                  double *restrict vec_out) {
   uint64_t t0, t1;
   t0 = get_timestamp_in_microsec();
   // https://www.intel.com/content/www/us/en/develop/documentation/onemkl-developer-reference-c/top/vector-mathematical-functions/vm-mathematical-functions/exponential-and-logarithmic-functions/v-ln.html#v-ln
@@ -16,8 +21,11 @@ __declspec(dllexport)  double mkl_ln(uint64_t arr_size, double *restrict vec_in,
   return (t1 - t0) / 1000.0;
 }
 
-__declspec(dllexport) double my_ln(uint64_t arr_size, double *restrict vec_in,
-             double *restrict vec_out) {
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+    double my_ln(uint64_t arr_size, double *restrict vec_in,
+                 double *restrict vec_out) {
   uint64_t t0, t1;
   t0 = get_timestamp_in_microsec();
 
