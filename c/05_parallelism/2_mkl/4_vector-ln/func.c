45b58fbb280b18018cdcfd1d20d4345ebb30227b
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-10-17 20:14:49 +0800

    make it Windows compatible

--- c/05_parallelism/2_mkl/4_vector-ln/func.c
+++ c/05_parallelism/2_mkl/4_vector-ln/func.c
@@ -1,16 +1,12 @@
+#include "../../utils.h"
+
 #include <mkl.h>
 
 #include <math.h>
 #include <stdint.h>
-#include <sys/time.h>
 
-uint64_t get_timestamp_in_microsec() {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return 1000000 * tv.tv_sec + tv.tv_usec;
-}
 
-double mkl_ln(uint64_t arr_size, double *restrict vec_in,
+__declspec(dllexport)  double mkl_ln(uint64_t arr_size, double *restrict vec_in,
               double *restrict vec_out) {
   uint64_t t0, t1;
   t0 = get_timestamp_in_microsec();
@@ -20,7 +16,7 @@ double mkl_ln(uint64_t arr_size, double *restrict vec_in,
   return (t1 - t0) / 1000.0;
 }
 
-double my_ln(uint64_t arr_size, double *restrict vec_in,
+__declspec(dllexport) double my_ln(uint64_t arr_size, double *restrict vec_in,
              double *restrict vec_out) {
   uint64_t t0, t1;
   t0 = get_timestamp_in_microsec();
@@ -30,9 +26,4 @@ double my_ln(uint64_t arr_size, double *restrict vec_in,
   }
   t1 = get_timestamp_in_microsec();
   return (t1 - t0) / 1000.0;
-}
-
-double my_dot_product(double *vec_a, double *vec_b, double *sum,
-                      int64_t arr_size) {
-  return 0;
 }
