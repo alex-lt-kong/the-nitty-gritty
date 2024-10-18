787532df82c4d1e8802073b61bda876da25ee36a
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-10-18 19:42:03 +0800

    remove paralleism

--- c/05_parallelism/1_generic/03_vectorization-in-o-files/func.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <stdint.h>
-
-#define SIZE 256
-
-void linear_func(uint8_t a, uint8_t b, uint8_t* arr, uint8_t* results, size_t arr_len);
