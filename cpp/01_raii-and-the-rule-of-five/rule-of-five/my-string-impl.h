4d981620ad6125d9a2594ee14675d01bdd7ddd56
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-03-17 00:08:19 +0800

    update rule of five

--- cpp/01_raii-and-the-rule-of-five/rule-of-five/my-string-impl.h
+++ cpp/01_raii-and-the-rule-of-five/rule-of-five/my-string-impl.h
@@ -44,65 +44,77 @@ public:
 
   // Move constructor
   my_string(my_string &&rhs) noexcept {
-    cout << "[Interal] move constructor called, rhs._str: [" << rhs.m_buf << "]"
-         << endl;
-    m_buf = rhs.m_buf; // "Ownership transfer"
+    m_capacity = rhs.m_capacity;
+    m_buf = rhs.m_buf;
     rhs.m_buf = nullptr;
+    rhs.m_capacity = 0;
+    std::println("Move ctor my_string(my_string &&rhs) called, "
+                 "m_capacity: {}, m_buf: [{}]",
+                 m_capacity, m_buf);
+  }
+
+  // Move constructor
+  my_string(char *&&rhs, std::size_t capacity) noexcept
+      : m_capacity(capacity), m_buf(rhs) {
+    rhs = nullptr;
+    std::println("Move ctor my_string(char *&&rhs) called, "
+                 "m_capacity: {}, m_buf: [{}]",
+                 m_capacity, m_buf);
   }
 
   // Copy assignment operator
   my_string &operator=(const my_string &rhs) {
     bool reallocation = false;
-    if (m_capacity < rhs.m_capacity) {
-      reset();
-      m_capacity = rhs.m_capacity;
-      m_buf = allocator.allocate(m_capacity);
-      reallocation = true;
-    } else {
-      // Let's memset() before memcpy() to avoid offset calculation here...
-      std::memset(m_buf, 0, m_capacity);
+    if (this != &rhs) {
+      if (m_capacity < rhs.m_capacity) {
+        reset();
+        m_capacity = rhs.m_capacity;
+        m_buf = allocator.allocate(m_capacity);
+        reallocation = true;
+      } else {
+        // Let's memset() before memcpy() to avoid offset calculation here...
+        std::memset(m_buf, 0, m_capacity);
+      }
+      std::memcpy(m_buf, rhs.m_buf, rhs.m_capacity);
     }
-    std::memcpy(m_buf, rhs.m_buf, m_capacity);
     std::println("Copy assignment operator=(const my_string &rhs) called, "
                  "reallocation: {}, m_capacity: {}, m_buf: [{}]",
                  reallocation, m_capacity, m_buf);
     return *this;
   }
 
-  my_string operator+(const my_string &rhs) {
-    cout << "[Interal] addition operator called, rhs._str: [" << rhs.m_buf
-         << "]" << endl;
-    char *temp = (char *)malloc(sizeof(char) *
-                                (strlen(rhs.m_buf) + strlen(this->m_buf) + 1));
-    if (temp == NULL) {
-      throw bad_alloc();
+  // Move assignment operator
+  my_string &operator=(my_string &&rhs) noexcept {
+    if (this != &rhs) { // self-assignment protection
+      reset();
+      m_capacity = rhs.m_capacity;
+      m_buf = rhs.m_buf;
+      rhs.m_capacity = 0;
+      rhs.m_buf = nullptr;
     }
-    strcpy(temp, this->m_buf);
-    strcpy(temp + strlen(this->m_buf), this->m_buf);
-
-    // memcpy(temp + strlen(this->buf), rhs.buf, strlen(rhs.buf));
-    my_string ns(temp);
-    free(temp);
-    return ns;
+    std::println(
+        "Move assignment my_string &operator=(my_string &&rhs) called, "
+        "m_capacity: {}, m_buf: [{}]",
+        m_capacity, m_buf);
+    return *this;
   }
 
-  /*
-    // Move assignment operator
-    my_string operator=(my_string &&rhs) noexcept {
-      cout << "[Interal] move assignment operator called, ";
-      if (this != &rhs) { // self-assignment protection
-        cout << "and it is making an impact, rhs._str: [" << rhs.m_buf << "]"
-             << endl;
-        free(this->m_buf);
-        this->m_buf = rhs.m_buf;
-        rhs.m_buf = nullptr;
-      } else {
-        cout << "but it is doing nothing, rhs._str: [" << rhs.m_buf << "]"
-             << endl;
-      }
-      return *this;
-    }
-  */
+  // Addition operator overloading typically returns a value
+  my_string operator+(const my_string &rhs) {
+    auto capacity = std::bit_ceil(strlen(m_buf) + strlen(rhs.m_buf) + 1);
+    auto buf = allocator.allocate(capacity);
+
+    strcpy(buf, this->m_buf);
+    strcat(buf, rhs.m_buf);
+
+    my_string ms{std::move(buf), capacity};
+    std::println(
+        "Addition operator my_string &operator=(my_string &&rhs) called, "
+        "ms.m_capacity: {}, ms.m_buf: [{}]",
+        ms.m_capacity, ms.m_buf);
+    return ms;
+  }
+
   bool operator==(const my_string &other) const {
     return strcmp(this->m_buf, other.m_buf) == 0;
   }
@@ -111,61 +123,11 @@ public:
     return strcmp(this->m_buf, other.c_str()) == 0;
   }
 
-  void PrintInternalPointerAddress() {
-    cout << "_str: " << (void *)m_buf << endl;
-  }
-
-  friend ostream &operator<<(ostream &os, const my_string &ns) {
-    os << ns.m_buf;
-    return os;
-  }
-
   char *c_str() const noexcept { return m_buf; }
 
-  std::size_t size() const noexcept { return strlen(m_buf); }
+  std::size_t size() const noexcept {
+    return m_capacity == 0 ? 0 : strlen(m_buf);
+  }
 
   ~my_string() { reset(); }
 };
-
-/*
-int main() {
-  cout << "Test 1\n";
-  my_string ns_hel((const char *)"Hello ");
-  auto ns_hel2 = ns_hel;
-  cout << "ns_hel2: " << ns_hel2 << endl << endl;
-
-  cout << "Test 2\n";
-  my_string ns_wor((char *)"world!");
-  cout << "ns_hel + ns_wor: " << ns_hel + ns_wor << endl << endl;
-
-  cout << "Test 3\n";
-  my_string ns_hw0 = ns_hel + ns_wor;
-  // This mostly triggers copy constructor with copy elision
-  cout << "ns_hw0: " << ns_hw0 << endl << endl;
-
-  cout << "Test 4\n";
-  my_string ns_hw1 = my_string((char *)"Hello world");
-  cout << "ns_hw1: " << ns_hw1 << "\n";
-  ns_hw1 = ns_hw1;
-  cout << "ns_hw1: " << ns_hw1 << "\n";
-  ns_hw1 = my_string((char *)"Goodbye");
-  cout << "ns_hw1: " << ns_hw1 << endl << endl;
-
-  cout << "Test 5\n";
-  my_string ns_fb = my_string((char *)"foobar");
-  cout << "ns_fb: " << ns_fb << "\n";
-  ns_fb = ns_hw1;
-  cout << "ns_fb: " << ns_fb << endl << endl;
-
-  cout << "Test 6\n";
-  my_string ns_fh = my_string((char *)"foobar") + my_string((char *)"Hello!");
-  cout << "ns_fh: " << ns_fh << endl << endl;
-
-  cout << "Test 7\n";
-  ns_fb.PrintInternalPointerAddress();
-  my_string ns_fb1 = std::move(ns_fb);
-  ns_fb.PrintInternalPointerAddress();
-  ns_fb1.PrintInternalPointerAddress();
-  return 0;
-}
-*/
