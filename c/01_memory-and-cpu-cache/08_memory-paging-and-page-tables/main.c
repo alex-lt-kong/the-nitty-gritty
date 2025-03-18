5db1b458a5445939b37cb46b04ff665c12a7e1a8
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-03-19 01:15:25 +0800

    updates on memory paging

--- c/01_memory-and-cpu-cache/08_memory-paging-and-page-tables/main.c
+++ c/01_memory-and-cpu-cache/08_memory-paging-and-page-tables/main.c
@@ -1,8 +1,9 @@
 // #include <print>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-void split_address(void *ptr, int page_size) {
+void split_address(void *ptr, long page_size) {
   unsigned long address = (unsigned long)ptr;  // Cast pointer to unsigned long
   int offset_bits = __builtin_ctz(page_size);  // Calculate bits for offset
   unsigned long page_offset = address & ((1UL << offset_bits) - 1);  // Mask offset bits
@@ -14,7 +15,10 @@ void split_address(void *ptr, int page_size) {
 }
 
 int main() {
-  int data = 42;  // Example variable
-  split_address(&data, 4096);  // Pass the pointer to the function
+  int data = 42;
+  const long page_size = sysconf(_SC_PAGESIZE);
+  printf("page_size: %ld\n", page_size);
+  printf("%ld\n", (long)(&data) % page_size);
+  split_address(&data, page_size);
   return 0;
 }
