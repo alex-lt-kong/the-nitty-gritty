afe3f37c18ac3d3ea33de2057d1522d5923c6b66
Alex Kong <alex.lt.kong@connect.ust.hk>
2025-04-21 20:18:57 +0800

    refine C++ polymorphism

--- cpp/02_polymorphism/function-overriding.h
+++ cpp/02_polymorphism/function-overriding.h
@@ -7,7 +7,7 @@ class Base {
 public:
   virtual ~Base() = default;
   virtual void show() { std::println("Base::show() called."); }
-  void showWithoutVirtual() {
+  /* no virtual here */ void showWithoutVirtual() {
     std::println("Base::showWithoutVirtual() called.");
   }
 };
@@ -15,11 +15,12 @@ public:
 class Derived : public Base {
 public:
   void show() override { std::println("Derived::show() called."); }
-  void showWithoutVirtual() {
+  void showWithoutVirtual() /* can't override */ {
     std::println("Derived::showWithoutVirtual() called.");
   }
 };
 
+// show() has no way to ascertain which show() to call at compile time
 inline void show(Base &obj) { obj.show(); }
 inline void showWithoutVirtual(Base &obj) { obj.showWithoutVirtual(); }
 
